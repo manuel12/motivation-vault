1f9270504f77abf13403c874a08bbacdb782178d
Manuel <manuelpinedacabeza@gmail.com>
2021-11-18 10:30:19 +0100

    Broke anchor element into multiple lines

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -2,7 +2,6 @@ import '../css/Navbar.css';
 
 function Navbar({ deleteToken }) {
   const logoutUser = () => {
-    console.log('logout')
     deleteToken()
     window.location.href = '/';
   }
@@ -12,22 +11,40 @@ function Navbar({ deleteToken }) {
     <div className="app-nav-bar">
       <ul className="nav-list">
         <li className="nav-item">
-          <a className="nav-item-link" href="/">Home</a>
+          <a
+            className="nav-item-link"
+            href="/"
+            data-test="home-link">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-item-link" href="/books/">Books</a>
+          <a
+            className="nav-item-link"
+            href="/books/"
+            data-test="books-link">Books</a>
         </li>
         <li className="nav-item">
-          <a className="nav-item-link" href="/podcasts/">Podcasts</a>
+          <a
+            className="nav-item-link"
+            href="/podcasts/"
+            data-test="podcasts-link">Podcasts</a>
         </li>
         <li className="nav-item">
-          <a className="nav-item-link" href="/motivational-speeches/">Motivational Speeches</a>
+          <a
+            className="nav-item-link"
+            href="/motivational-speeches/"
+            data-test="motivational-speeches-link">Motivational Speeches</a>
         </li>
         <li className="nav-item">
-          <a className="nav-item-link" href="/about/">About</a>
+          <a
+            className="nav-item-link"
+            href="/about/"
+            data-test="about-link">About</a>
         </li>
         <li className="nav-item">
-          <div className="nav-item-link " onClick={()=> {logoutUser()}}>Logout</div>
+          <div
+            className="nav-item-link "
+            onClick={()=> {logoutUser()}}
+            data-test="logout-link">Logout</div>
         </li>
       </ul>
     </div>
