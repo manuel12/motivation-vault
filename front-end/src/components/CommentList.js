143c66672225475bece18ac1ae454aeca66309d3
Manuel <manuelpinedacabeza@gmail.com>
2021-11-19 15:21:30 +0100

    Added comments, setComments, commentText and setCommentText states. Added useEffect to setComments. Added submit btn functionality. Added submit btn. Changed from using props.comments arr to using comments arr.

--- front-end/src/components/CommentList.js
+++ front-end/src/components/CommentList.js
@@ -1,8 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Comment from './Comment';
 import "../css/CommentList.css"
 
 function CommentList(props) {
+  let [comments, setComments] = useState([]);
+  let [commentText, setCommentText] = useState('');
 
+  useEffect(() => setComments(props.comments), [props.comments])
+
+  const submitClicked = () => {
+    const commentInput = document.getElementsByClassName('add-comment')[0];
+    commentInput.value = ''
+
+    if(commentText) {
+      const newComment = {
+        'user': 'Manuel',
+        'text': commentText,
+        'date': new Date().toISOString()
+
+      }
+      const updatedComments = [newComment, ...comments];
+      setComments(updatedComments);
+      setCommentText('');
+    }
+  }
   return (
     <div
       className="comment-list-container"
@@ -14,11 +35,20 @@ function CommentList(props) {
         className="add-comment"
         type="text"
         placeholder="Write a comment..."
+        onChange={ evt => {
+          setCommentText(evt.target.value)
+          }
+        }
         data-test="comment-input"/>
 
+      <input
+        id="submit"
+        type="submit"
+        onClick={submitClicked}
+        data-test="submit"/>
       <br/>
 
-      {props.comments && props.comments.map((comment) =>(
+      {comments && comments.map((comment) =>(
         <div>
           <Comment
             key={comment.id}
@@ -28,6 +58,7 @@ function CommentList(props) {
           />
         </div>
       ))}
+
     </div>
   )
 }
