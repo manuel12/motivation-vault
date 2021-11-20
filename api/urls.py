0354d22e9be14fb8309e36957e249a0efa91fd8e
Manuel <manuelpinedacabeza@gmail.com>
2021-11-20 16:59:57 +0100

    Updated views classes

--- api/urls.py
+++ api/urls.py
@@ -6,24 +6,23 @@ router = routers.SimpleRouter()
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-  path('', views.ResourceListView.as_view()),
-  path('<int:pk>/', views.ResourceDetailView.as_view()),
+  path('', views.ResourceList.as_view()),
+  path('<int:pk>/', views.ResourceDetail.as_view()),
 
-  path('books/', views.BookAPIView.as_view()),
-  path('books/<int:pk>/', views.BookDetailAPIView.as_view()),
-  
-  path('podcasts/', views.PodcastAPIView.as_view()),
-  path('podcasts/<int:pk>/', views.PodcastDetailAPIView.as_view()),
-  
-  path('podcasts-episodes/', views.PodcastEpisodeAPIView.as_view()),
-  path('podcasts-episodes/<int:pk>/', views.PodcastDetailAPIView.as_view()),
+  path('books/', views.BookList.as_view()),
+  path('books/<int:pk>/', views.BookDetail.as_view()),
 
-  path('motivational-speeches/', views.MotivationalSpeechAPIView.as_view()),
-  path('motivational-speeches/<int:pk>/', views.MotivationalSpeechDetailAPIView.as_view()),
+  path('podcasts/', views.PodcastList.as_view()),
+  path('podcasts/<int:pk>/', views.PodcastDetail.as_view()),
+  
+  path('podcasts-episodes/', views.PodcastEpisodeList.as_view()),
+  path('podcasts-episodes/<int:pk>/', views.PodcastEpisodeDetail.as_view()),
+
+  path('motivational-speeches/', views.MotivationalSpeechList.as_view()),
+  path('motivational-speeches/<int:pk>/', views.MotivationalSpeechDetail.as_view()),
 
   path('comments/', views.CommentsAPIView.as_view()),
   path('ratings/', views.RatingsAPIView.as_view()),
-
 ]
 
 urlpatterns += router.urls
