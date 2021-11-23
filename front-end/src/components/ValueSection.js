b0088bb4bc0a1d9b1e2de3387467dfc3458a28b6
Manuel <manuelpinedacabeza@gmail.com>
2021-11-23 19:27:45 +0100

    Added ValueSection

---
+++ front-end/src/components/ValueSection.js
@@ -0,0 +1,16 @@
+function ValueSection(props) {
+  return (
+    <div 
+      className="value-section-container"
+      data-test="value-section">
+      <h3>What value does it bring you?</h3>
+        <ul>
+          <li>{props.resource.value_one}</li>
+          <li>{props.resource.value_two}</li>
+          <li>{props.resource.value_three}</li>
+        </ul>
+    </div>
+  )
+}
+
+export default ValueSection;
