18de0f29b1c6b1ad4a981fe4c52bf9c8bcb7369f
Manuel <manuelpinedacabeza@gmail.com>
2021-11-23 19:31:44 +0100

    Removed token and cookies from component.

--- front-end/src/components/MotivationalSpeechpage.js
+++ front-end/src/components/MotivationalSpeechpage.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import PostList from './PostList';
 import { API  } from '../api-service';
-import { useCookies } from 'react-cookie';
-
 function MotivationalSpeechPage() {
   let [resources, setResources] = useState([]);
-  const [token] = useCookies(['mr-token']);
 
   useEffect(() => {
     API.fetchResource({
       resource: 'motivational-speeches', 
-      setResourceFunc: setResources,
-      token: {token}})
-  }, [token])
+      setResourceFunc: setResources})
+  }, [])
 
   return (
     <div>
