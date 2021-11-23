3e875337cf824801d34bac2b2d0548ff6e70233b
Manuel <manuelpinedacabeza@gmail.com>
2021-11-23 19:32:12 +0100

    Removed token and cookies from component.

--- front-end/src/components/PodcastEpisodepage.js
+++ front-end/src/components/PodcastEpisodepage.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import PostList from './PostList';
 import { API  } from '../api-service';
-import { useCookies } from 'react-cookie';
 
 function PodcastEpisodePage() {
   let [resources, setResources] = useState([]);
-  const [token] = useCookies(['mr-token']);
 
   useEffect(() => {
     API.fetchResource({
       resource: 'podcasts-episodes', 
-      setResourceFunc: setResources,
-      token: token})
-  }, [token])
+      setResourceFunc: setResources})
+  }, [])
 
   console.log(resources)
   return (
