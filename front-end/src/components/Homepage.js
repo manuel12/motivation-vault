5cf5ef15f601c2f8144410b277ff9f84c74b0495
Manuel <manuelpinedacabeza@gmail.com>
2021-11-23 19:31:11 +0100

    Removed token and cookies from component.

--- front-end/src/components/Homepage.js
+++ front-end/src/components/Homepage.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import PostList from './PostList';
 import { API  } from '../api-service';
-import { useCookies } from 'react-cookie';
 
 function HomePage(props) {
   let [resources, setResources] = useState([]);
-  const [token] = useCookies(['mr-token']);
 
   useEffect(() => {
     API.fetchResource({
       resource: 'home', 
-      setResourceFunc: setResources,
-      token: token})
-  }, [token])
+      setResourceFunc: setResources})
+  }, [])
 
   return (
     <div>
