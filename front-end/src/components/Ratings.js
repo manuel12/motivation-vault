1959849fcb46ed229c8826838aa5d517fec139b5
Manuel <manuelpinedacabeza@gmail.com>
2021-11-27 17:11:26 +0100

    Imported useState, and faPlusSquare icon. Added showNumberInput, setShowNumberInput and rating, setRating states. Added handleClick and handleSubmitRating funcs. Added POST request. Added avgRating and numRatings variables. Added data-test attributes to elements

--- front-end/src/components/Ratings.js
+++ front-end/src/components/Ratings.js
@@ -1,26 +1,105 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar} from '@fortawesome/free-solid-svg-icons';
-import "../css/Ratings.css"
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import "../css/Ratings.css";
 
-function Rating(props) {
+function Ratings(props) {
+  let [showNumberInput, setShowNumberInput] = useState(false);
+  let [rating, setRating] = useState(1);
+
+  const handleClick = () => {
+    setShowNumberInput(true);
+  };
+
+  const handleSubmitRating = () => {
+    const newRating = {
+      resource: props.resourceId,
+      stars: rating,
+    };
+
+    fetch(`http://127.0.0.1:8000/api/ratings/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token 51d55878caa6db7066be358ad1cd51eb90d88897`,
+      },
+      body: JSON.stringify(newRating),
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((error) => console.warn(error));
+  };
+
+  let avgRating = props.avgRating;
+  let numRatings = props.numRatings;
 
   return (
-    props.rating ? 
-      <div 
-        className="ratings-container"
-        data-test="ratings-container">
-          <FontAwesomeIcon icon={faStar}
-            className={props.rating > 0 ? 'orange' : 'white'}/>
-          <FontAwesomeIcon icon={faStar}
-            className={props.rating > 1 ? 'orange' : 'white'}/>
-          <FontAwesomeIcon icon={faStar}
-            className={props.rating > 2 ? 'orange' : 'white'}/>
-          <FontAwesomeIcon icon={faStar}
-            className={props.rating > 3 ? 'orange' : 'white'}/>
-          <FontAwesomeIcon icon={faStar}
-            className={props.rating > 4 ? 'orange' : 'white'}/>
-    </div> : null
-  )
+    <div className="ratings-container" data-test="ratings-container">
+      <FontAwesomeIcon
+        icon={faStar}
+        className={avgRating > 0 ? "orange" : "white"}
+        data-test={`star-icon-${1}`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={avgRating > 1 ? "orange" : "white"}
+        data-test={`star-icon-${2}`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={avgRating > 2 ? "orange" : "white"}
+        data-test={`star-icon-${3}`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={avgRating > 3 ? "orange" : "white"}
+        data-test={`star-icon-${4}`}
+      />
+      <FontAwesomeIcon
+        icon={faStar}
+        className={avgRating > 4 ? "orange" : "white"}
+        data-test={`star-icon-${5}`}
+      />
+
+      <span className="num-ratings" data-test="num-ratings">
+        (
+        {`${numRatings} 
+            ${numRatings !== 1 ? "reviews" : "review"}`}
+        )
+      </span>
+      {props.addRatingBtn ? (
+        <FontAwesomeIcon
+          icon={faPlusSquare}
+          className="add-rating"
+          onClick={handleClick}
+          data-test="add-rating-button"
+        />
+      ) : null}
+      {showNumberInput ? (
+        <div>
+          <input
+            type="number"
+            max="5"
+            min="1"
+            defaultValue="1"
+            onChange={(evt) => {
+              setRating(evt.target.value);
+            }}
+            data-test="add-rating-input"
+          />
+          <button 
+            type="button" 
+            onClick={handleSubmitRating}
+            data-test="add-rating-submit-button">
+            Submit
+          </button>
+        </div>
+      ) : null}
+    </div>
+    // : null
+  );
 }
 
-export default Rating;
+export default Ratings;
