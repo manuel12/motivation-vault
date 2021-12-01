bf770ad90902fc52de32b0c566b6aecd48170e7c
Manuel <manuelpinedacabeza@gmail.com>
2021-12-01 17:30:36 +0100

    Reformated code

--- front-end/src/components/Bookpage.js
+++ front-end/src/components/Bookpage.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import PostList from './PostList';
-import { API  } from '../api-service';
+import React, { useState, useEffect } from "react";
+import PostList from "./PostList";
+import { API } from "../api-service";
 
 function BookPage() {
   let [resources, setResources] = useState([]);
 
   useEffect(() => {
     API.fetchResource({
-      resource: 'books', 
-      setResourceFunc: setResources})
-  }, [])
-  
+      resource: "books",
+      setResourceFunc: setResources,
+    });
+  }, []);
+
   return (
     <div>
-      <PostList resources={resources}/>
+      <PostList resources={resources} />
     </div>
-  )
+  );
 }
 
 export default BookPage;
