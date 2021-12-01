82b39eeaac327392bddf73259307d2118bdfba9a
Manuel <manuelpinedacabeza@gmail.com>
2021-12-01 17:30:58 +0100

    Reformated code

--- front-end/src/components/Input.js
+++ front-end/src/components/Input.js
@@ -1,22 +1,23 @@
-import '../css/Input.css';
+import "../css/Input.css";
 
 function Input(props) {
-  
-  return(
+  return (
     <div className="inputContainer">
-      {props.error ?
-        <label data-test={`${props?.dataAttr}-error`}>*{props.error}</label> : null}
-      <input  
-        id={props.item?.toLowerCase()} 
+      {props.error ? (
+        <label data-test={`${props?.dataAttr}-error`}>*{props.error}</label>
+      ) : null}
+      <input
+        id={props.item?.toLowerCase()}
         type={props.itemType}
         placeholder={props?.item}
         defaultValue={props?.defaultValue}
         value={props.value}
         onChange={props.onChange}
-        data-test={props?.dataAttr}/> 
-        <br/>
+        data-test={props?.dataAttr}
+      />
+      <br />
     </div>
-  )
+  );
 }
 
 export default Input;
