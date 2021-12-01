a98f841f0ab3fa885959db6498fe86e7cb27d0c2
Manuel <manuelpinedacabeza@gmail.com>
2021-12-01 17:26:57 +0100

    Updated Rating avgRating prop. Passed updateResource prop. Reformated code.

--- front-end/src/components/Detailpage.js
+++ front-end/src/components/Detailpage.js
@@ -1,28 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { API } from '../api-service';
-import CommentSection from './CommentSection';
-import Rating from './Ratings';
-import ValueSection from './ValueSection';
-import "../css/Detailpage.css"
+import { API } from "../api-service";
+import CommentSection from "./CommentSection";
+import Rating from "./Ratings";
+import ValueSection from "./ValueSection";
+import "../css/Detailpage.css";
 
 function DetailPage() {
-  const { id } = useParams()
+  const { id } = useParams();
   let [resource, setResource] = useState([]);
 
   useEffect(() => {
     API.fetchResource({
-      resource: null, 
-      setResourceFunc: setResource, 
-      id: id})
-    },[id])
+      resource: null,
+      setResourceFunc: setResource,
+      id: id,
+    });
+  }, [id]);
 
   return (
     <div className="container">
       <div className="post-section-container">
-        <h1 className="heading">
-          {resource.title}
-        </h1>
+        <h1 className="heading">{resource.title}</h1>
         <div className="author-container">
           By <strong>{resource.author}</strong>
         </div>
@@ -30,22 +29,18 @@ function DetailPage() {
           resourceId={resource.id}
           avgRating={resource.avg_rating}
           numRatings={resource.num_ratings}
-          addRatingBtn={true}/>
-        <img 
-          src={resource.imageURL}
-          className="image"
-          alt=""></img>
+          updateResource={setResource}
+          addRatingBtn={true}
+        />
+        <img src={resource.imageURL} className="image" alt=""></img>
         <h3>Description</h3>
         <p className="paragraph-container">{resource.description}</p>
       </div>
 
-      <ValueSection
-        resource={resource}/>
-      <CommentSection
-        resourceId={id}
-        comments={resource.get_comments}/>
+      <ValueSection resource={resource} />
+      <CommentSection resourceId={id} comments={resource.get_comments} />
     </div>
-  )
+  );
 }
 
 export default DetailPage;
