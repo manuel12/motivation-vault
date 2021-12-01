5ca3b80610450c08fd81cb118a76cdbf3dc6d6e3
Manuel <manuelpinedacabeza@gmail.com>
2021-12-01 17:30:44 +0100

    Reformated code

--- front-end/src/components/Comment.js
+++ front-end/src/components/Comment.js
@@ -1,18 +1,15 @@
-import "../css/Comment.css"
+import "../css/Comment.css";
 
 function Comment(props) {
-
-    return (
-      <div
-        className="comment-container"
-        data-test="comment-container">
-        <div>
-          <strong>{props.user}</strong> - {props.date}
-        </div>
-        <div>{props.text}</div> 
-        <br/>
+  return (
+    <div className="comment-container" data-test="comment-container">
+      <div>
+        <strong>{props.user}</strong> - {props.date}
       </div>
-    )
+      <div>{props.text}</div>
+      <br />
+    </div>
+  );
 }
 
 export default Comment;
