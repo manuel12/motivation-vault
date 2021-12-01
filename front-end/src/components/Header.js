bd12fe6e6dd6350433c76dce3f445e7025f24391
Manuel <manuelpinedacabeza@gmail.com>
2021-12-01 17:30:54 +0100

    Reformated code

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,16 +1,15 @@
-import '../css/Header.css';
+import "../css/Header.css";
 
 function Header(props) {
   return (
     <div>
       <header className="app-header">
-        <a className="header-link"  href="/" >
+        <a className="header-link" href="/">
           <h1>Resource API Project</h1>
         </a>
       </header>
     </div>
-
-  )
+  );
 }
 
 export default Header;
