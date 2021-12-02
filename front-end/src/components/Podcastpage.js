c8310b3ebac09aceff0ba87288ae3e9461cf70be
Manuel <manuelpinedacabeza@gmail.com>
2021-12-02 19:57:53 +0100

    Reformated code

--- front-end/src/components/Podcastpage.js
+++ front-end/src/components/Podcastpage.js
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import PostList from './PostList';
-import { API  } from '../api-service';
+import React, { useState, useEffect } from "react";
+import PostList from "./PostList";
+import { API } from "../api-service";
 
 function PodcastPage() {
   let [resources, setResources] = useState([]);
 
   useEffect(() => {
     API.fetchResource({
-      resource: 'podcasts', 
-      setResourceFunc: setResources})
-  }, [])
+      resource: "podcasts",
+      setResourceFunc: setResources,
+    });
+  }, []);
 
   return (
     <div>
-      <PostList resources={resources}/>
+      <PostList resources={resources} />
     </div>
-  )
+  );
 }
 
 export default PodcastPage;
