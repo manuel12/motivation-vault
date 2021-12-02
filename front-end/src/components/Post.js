7c55e2131ab000655a4f10ac9aaf48569f4f753f
Manuel <manuelpinedacabeza@gmail.com>
2021-12-02 19:57:59 +0100

    Reformated code

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -1,35 +1,28 @@
-import Ratings from './Ratings';
-import "../css/Post.css"
-
+import Ratings from "./Ratings";
+import "../css/Post.css";
 
 function Post(props) {
-
-    return (
-      <div
-        className="post-container"
-        data-test="post-container">
-        <a href={`http://localhost:3000/${props.id}/`} className="container-link">
-          <div className="image-container">
-            <img src={props.imageURL} className="image" alt=""></img>
+  return (
+    <div className="post-container" data-test="post-container">
+      <a href={`http://localhost:3000/${props.id}/`} className="container-link">
+        <div className="image-container">
+          <img src={props.imageURL} className="image" alt=""></img>
+        </div>
+        <div className="text-container">
+          <h3>{props.title}</h3>
+          <div className="author-text-container">
+            by <h5 className="author-name">{props.author}</h5>
           </div>
-          <div className="text-container">
-            <h3>{props.title}</h3>
-            <div className="author-text-container">
-              by <h5 className="author-name">{props.author}</h5>
-            </div>
-            <Ratings
-              avgRating={props.avgRating}
-              numRatings={props.numRatings}/>
-            <p className="description-container">{
-              props.description &&
-              props.description.length > 300 ?
-              `${props.description.substring(0,300)}...` :
-              props.description
-              }</p>
-          </div>
-        </a>
-      </div>
-    )
+          <Ratings avgRating={props.avgRating} numRatings={props.numRatings} />
+          <p className="description-container">
+            {props.description && props.description.length > 300
+              ? `${props.description.substring(0, 300)}...`
+              : props.description}
+          </p>
+        </div>
+      </a>
+    </div>
+  );
 }
 
 export default Post;
