e8d530495b16ad00b3d3a77897c63c37cc961c89
Manuel <manuelpinedacabeza@gmail.com>
2021-12-02 19:58:05 +0100

    Reformated code

--- front-end/src/components/PostList.js
+++ front-end/src/components/PostList.js
@@ -1,24 +1,22 @@
-import Post from './Post';
+import Post from "./Post";
 
 function PostList(props) {
-
   return (
     <div>
-      {props.resources.map((resource) =>(
-        <Post 
-        key={resource.id}
-        id={resource.id}
-        title={resource.title}
-        author={resource.author}
-        description={resource.description}
-        imageURL={resource.imageURL}
-        avgRating={resource.avg_rating}
-        numRatings={resource.num_ratings}
+      {props.resources.map((resource) => (
+        <Post
+          key={resource.id}
+          id={resource.id}
+          title={resource.title}
+          author={resource.author}
+          description={resource.description}
+          imageURL={resource.imageURL}
+          avgRating={resource.avg_rating}
+          numRatings={resource.num_ratings}
         />
       ))}
     </div>
-  )
+  );
 }
 
 export default PostList;
-
