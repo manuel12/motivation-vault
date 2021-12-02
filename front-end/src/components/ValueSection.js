365fec01d699bd8a2c75698cf70a4bb76ba683ce
Manuel <manuelpinedacabeza@gmail.com>
2021-12-02 19:58:20 +0100

    Reformated code

--- front-end/src/components/ValueSection.js
+++ front-end/src/components/ValueSection.js
@@ -1,16 +1,14 @@
 function ValueSection(props) {
   return (
-    <div 
-      className="value-section-container"
-      data-test="value-section">
+    <div className="value-section-container" data-test="value-section">
       <h3>What value does it bring you?</h3>
-        <ul>
-          <li>{props.resource.value_one}</li>
-          <li>{props.resource.value_two}</li>
-          <li>{props.resource.value_three}</li>
-        </ul>
+      <ul>
+        <li>{props.resource.value_one}</li>
+        <li>{props.resource.value_two}</li>
+        <li>{props.resource.value_three}</li>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default ValueSection;
