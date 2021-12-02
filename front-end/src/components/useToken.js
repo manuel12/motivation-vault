a33c85c896e0ff81f8591dd228f34189afe74271
Manuel <manuelpinedacabeza@gmail.com>
2021-12-02 19:58:13 +0100

    Reformated code

--- front-end/src/components/useToken.js
+++ front-end/src/components/useToken.js
@@ -1,30 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function useToken() {
   const getToken = () => {
-    const tokenString = localStorage.getItem('token');
+    const tokenString = localStorage.getItem("token");
 
     try {
       const userToken = JSON.parse(tokenString);
       return userToken;
-    } catch(e) {
+    } catch (e) {
       return false;
     }
-  }
-  
+  };
+
   const [token, setToken] = useState(getToken());
 
   const saveToken = (userToken) => {
-    localStorage.setItem('token', JSON.stringify(userToken));
+    localStorage.setItem("token", JSON.stringify(userToken));
     setToken(userToken);
-  }
+  };
 
-  const deleteToken = () => localStorage.removeItem('token') 
+  const deleteToken = () => localStorage.removeItem("token");
 
   return {
     token,
     deleteToken,
     setToken: saveToken,
-  }
+  };
 }
-
