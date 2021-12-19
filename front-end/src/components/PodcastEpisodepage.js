7c1e20aae2573238c45c4477acde2a5332264ec9
Manuel <manuelpinedacabeza@gmail.com>
2021-12-19 16:52:48 +0100

    Imported and used token.

--- front-end/src/components/PodcastEpisodepage.js
+++ front-end/src/components/PodcastEpisodepage.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import PostList from "./PostList";
 import { API } from "../api-service";
+import useToken from './useToken';
 
 function PodcastEpisodePage() {
+  const {token} = useToken();
   let [resources, setResources] = useState([]);
 
   useEffect(() => {
     API.fetchResource({
       resource: "podcast-episodes",
+      token: token,
       setResourceFunc: setResources,
     });
-  }, []);
+  }, [token]);
 
   return (
     <div>
