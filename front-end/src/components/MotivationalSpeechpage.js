efd94d1a23c88029979b63fc69f500fe2fa80384
Manuel <manuelpinedacabeza@gmail.com>
2021-12-19 16:52:36 +0100

    Imported and used token.

--- front-end/src/components/MotivationalSpeechpage.js
+++ front-end/src/components/MotivationalSpeechpage.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import PostList from "./PostList";
 import { API } from "../api-service";
+import useToken from './useToken';
+
 function MotivationalSpeechPage() {
+  const {token} = useToken();
   let [resources, setResources] = useState([]);
 
   useEffect(() => {
     API.fetchResource({
       resource: "motivational-speeches",
+      token: token,
       setResourceFunc: setResources,
     });
-  }, []);
+  }, [token]);
 
   return (
     <div>
