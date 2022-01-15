ffc0f23c60aad69f90213a947fd210cc0cb7e47b
Manuel <manuelpinedacabeza@gmail.com>
2022-01-15 19:50:20 +0100

    Formatted page

--- front-end/src/components/MotivationalSpeechpage.js
+++ front-end/src/components/MotivationalSpeechpage.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import PostList from "./PostList";
 import { API } from "../api-service";
-import useToken from './useToken';
+import useToken from "./useToken";
 
 function MotivationalSpeechPage() {
-  const {token} = useToken();
+  const { token } = useToken();
   let [resources, setResources] = useState([]);
 
   useEffect(() => {
