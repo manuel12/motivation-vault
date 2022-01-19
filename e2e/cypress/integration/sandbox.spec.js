f2bfe4e4929ac5c1e69a323c4f66c44933ce49ee
Manuel <manuelpinedacabeza@gmail.com>
2022-01-19 18:13:06 +0100

    Added beforeEach hool and test

--- e2e/cypress/integration/sandbox.spec.js
+++ e2e/cypress/integration/sandbox.spec.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
 describe("Sandbox experiments", () => {
-  it("tries somehting cool", () => {
-    
-  })
-})
+  beforeEach(() => {
+    cy.visit("/");
+    cy.loginWithAPI("testuser1", "testpass1");
+  });
+
+  it("About page - should display about page", () => {
+    cy.get("[data-test=about-link]").click();
+    cy.get(".about-page-container").should("be.visible");
+    cy.matchImageSnapshot();
+  });
+});
