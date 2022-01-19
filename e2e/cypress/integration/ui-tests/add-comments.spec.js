178028c261b8f0b29c11cde97b6c76c9eef5ee59
Manuel <manuelpinedacabeza@gmail.com>
2022-01-19 18:22:25 +0100

    Removed before hook, added beforeEach. Addewd API request. Added should clear comment input form after clicking cancel button test. Updated text check.

--- e2e/cypress/integration/ui-tests/add-comments.spec.js
+++ e2e/cypress/integration/ui-tests/add-comments.spec.js
@@ -1,41 +1,31 @@
 /// <reference types="cypress" />
 
 describe("Add Comments", () => {
-  before(() => {
-    cy.deleteTestData()
-    cy.loginWithAPI("testuser1", "testpass1")
+  beforeEach(() => {
+    cy.deleteTestData();
+
     cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("book", resourceData);
-    })
-    cy.get(".post-container a").first().click({ force: true });
-  })
-
-  it("should add 3 comments", () => {
-    cy.fixture("comments").then((comments) => {
-      for (let comment of comments) {
-        cy.addCommentWithUI(comment.text)
-      }
+      cy.addResourceWithAPI("book", resourceData);
+    });
+    cy.request({
+      url: "http://localhost:8000/api/books/",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${Cypress.env("adminToken")}`,
+      },
+    }).then((response) => {
+      const firstBook = response.body[0];
+      expect(firstBook).to.have.property("id");
+      expect(firstBook).to.have.property("title", "Test Title");
     });
 
-    cy.get("[data-test=comment-container]")
-      .should("be.visible")
-      .and("have.length", 3);
-  });
-
-  it("should display comment text and user", () => {
-    cy.fixture("comments").then((comments) => {
-      for (let comment of comments) {
-        let commentIndex = comments.reverse().indexOf(comment);
-        cy.get("[data-test=comment-container]")
-          .eq(commentIndex)
-          .should("contain.text", comments[commentIndex].get_username)
-          .and("contain.text", comments[commentIndex].text);
-      }
-    });
+    cy.loginWithAPI("testuser1", "testpass1");
+    cy.contains("Test Title").click({ force: true });
   });
 
   it("should add a comment", () => {
-    cy.addCommentWithUI("[Test comment]")
+    cy.addCommentWithUI("[Test comment]");
 
     cy.get("[data-test=comment-container]")
       .first()
@@ -43,23 +33,48 @@ describe("Add Comments", () => {
   });
 
   it("should clear comment input after clicking submit", () => {
-    cy.addCommentWithUI("[Test comment]")
-    
+    cy.addCommentWithUI("[Test comment]");
+
     cy.get("[data-test=comment-input]").should(
       "not.contain.value",
       "[Test comment]"
     );
   });
 
+  it("should clear comment input form after clicking cancel button", () => {
+    cy.get("[data-test=comment-input]").type("[Test comment]");
+    cy.get("[data-test=add-comment-cancel-button]").click();
+    cy.get("[data-test=comment-input]").should("contain.value", "");
+  });
+
+  it("should add 5 comments", () => {
+    cy.fixture("comments").then((comments) => {
+      for (let comment of comments) {
+        cy.addCommentWithUI(comment.text);
+      }
+    });
+
+    cy.get("[data-test=comment-container]")
+      .should("be.visible")
+      .and("have.length", 5);
+  });
+
   it("should have comments persist after page reload", () => {
+    cy.fixture("comments").then((comments) => {
+      for (let comment of comments) {
+        cy.addCommentWithUI(comment.text);
+      }
+    });
+
+    cy.reload();
     cy.get("[data-test=comment-container]").should("have.length", 5);
 
     cy.get("[data-test=comment-container]")
       .eq(0)
-      .should("contain.text", "[Test comment]");
+      .should("contain.text", "This is my fifth comment!");
     cy.get("[data-test=comment-container]")
       .eq(1)
-      .should("contain.text", "[Test comment]");
+      .should("contain.text", "This is the forth comment!");
     cy.get("[data-test=comment-container]")
       .eq(2)
       .should("contain.text", "A truly great book, worth the read");
@@ -69,6 +84,7 @@ describe("Add Comments", () => {
     cy.get("[data-test=comment-container]")
       .eq(4)
       .should("contain.text", "Awesome book, really inspiring!");
+
   });
 
   after(() => {
