7996fdf98f9f6e4319b08c68695747ac39414bfd
Manuel <manuelpinedacabeza@gmail.com>
2022-02-21 19:21:59 +0100

    Removed repeated include import

--- api_project/urls.py
+++ api_project/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
