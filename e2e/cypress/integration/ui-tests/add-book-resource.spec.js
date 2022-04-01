9ae3977729aa85b15b4fa75030b665a228996d44
Manuel <manuelpinedacabeza@gmail.com>
2022-04-01 15:47:05 +0200

    Required fixtures, remove us of callbacks. Added loginAndCleanUp func. Changed classes for data attributes.

--- e2e/cypress/integration/ui-tests/add-book-resource.spec.js
+++ e2e/cypress/integration/ui-tests/add-book-resource.spec.js
@@ -1,55 +1,51 @@
 /// <reference types="cypress" />
 
+const resourceTestData = require("../../fixtures/resourceTestData.json");
+const resourceInvalidTestData = require("../../fixtures/invalidResourceTestData.json");
+
 describe("Add Book Resources", () => {
   beforeEach(() => {
-    cy.deleteTestData();
-    
-    cy.loginWithAPI("testuser1", "testpass1");
-    cy.get(".App").should("be.visible");
+    cy.loginAndCleanUp();
+    cy.get("[data-test=app]").should("be.visible");
   });
 
   it("should add a book resource", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("book", resourceData);
+    cy.addResourceWithUI("book", resourceTestData);
 
-      cy.url().should("not.contain", "add/");
-      cy.get(".add-container").should("not.exist");
+    cy.url().should("not.contain", "add/");
+    cy.get("[data-test=add-container]").should("not.exist");
 
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author)
-        .and("contain", resourceData.description);
-    });
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author)
+      .and("contain", resourceTestData.description);
   });
 
   it("should display added book resource on book section", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("book", resourceData);
+    cy.addResourceWithUI("book", resourceTestData);
 
-      cy.visit("/books/");
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author)
-        .and("contain", resourceData.description);
-    });
-  })
+    cy.visit("/books/");
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author)
+      .and("contain", resourceTestData.description);
+  });
 
   it("should add a book resource filling required fields only", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("book", resourceData, true);
+    cy.addResourceWithUI("book", resourceTestData, true);
 
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author);
-    });
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author);
   });
 
   it("should NOT submit a book form without filling required fields", () => {
     cy.visit("/add/");
-    cy.get(".add-container").should("be.visible");
+    cy.get("[data-test=add-container]").should("be.visible");
+
     cy.get("[data-test=select-resource-type]").select("Book");
 
     cy.get("[data-test=submit]").click();
@@ -63,45 +59,40 @@ describe("Add Book Resources", () => {
   });
 
   it("should NOT add a book resource with invalid data", () => {
-    cy.fixture("invalidResourceData").then((invalidData) => {
-      cy.addResourceWithUI("book", invalidData);
+    cy.addResourceWithUI("book", resourceInvalidTestData);
 
-      cy.url().should("contain", "add/");
-      
-      cy.get("[data-test=isbn-input-error]")
-        .should("be.visible")
-        .and("contain.text", "ISBN has to be a 13 digits!");
-    });
+    cy.url().should("contain", "add/");
+
+    cy.get("[data-test=isbn-input-error]")
+      .should("be.visible")
+      .and("contain.text", "ISBN has to be a 13 digits!");
   });
 
   it("should add a book resource by pressing ENTER when all fields are filled", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.visit("/add/");
-      cy.get(".add-container").should("be.visible");
-      cy.get("[data-test=select-resource-type]").select("Book");
+    cy.visit("/add/");
+    cy.get("[data-test=select-resource-type]").select("Book");
 
-      cy.get("[data-test=title-input]").type(resourceData.title);
-      cy.get("[data-test=subtitle-input]").type(resourceData.subtitle);
-      cy.get("[data-test=author-input]").type(resourceData.author);
-      cy.get("[data-test=isbn-input]").type(resourceData.isbn);
+    cy.get("[data-test=title-input]").type(resourceTestData.title);
+    cy.get("[data-test=subtitle-input]").type(resourceTestData.subtitle);
+    cy.get("[data-test=author-input]").type(resourceTestData.author);
+    cy.get("[data-test=isbn-input]").type(resourceTestData.isbn);
 
-      cy.get("[data-test=description-input]").type(resourceData.description);
+    cy.get("[data-test=description-input]").type(resourceTestData.description);
 
-      cy.get("[data-test=value-one-input]").type(resourceData.valueOne);
-      cy.get("[data-test=value-two-input]").type(resourceData.valueTwo);
-      cy.get("[data-test=value-three-input]").type(
-        `${resourceData.valueThree}{enter}`
-      );
+    cy.get("[data-test=value-one-input]").type(resourceTestData.valueOne);
+    cy.get("[data-test=value-two-input]").type(resourceTestData.valueTwo);
+    cy.get("[data-test=value-three-input]").type(
+      `${resourceTestData.valueThree}{enter}`
+    );
 
-      cy.url().should("not.contain", "add/");
-      cy.get(".add-container").should("not.exist");
+    cy.url().should("not.contain", "add/");
+    cy.get("[data-test=add-container]").should("not.exist");
 
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author)
-        .and("contain", resourceData.description);
-    });
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author)
+      .and("contain", resourceTestData.description);
   });
 
   after(() => {
