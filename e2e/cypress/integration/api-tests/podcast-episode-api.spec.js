fad0c084d0f4b6a85e5499388446fe580685bb50
Manuel <manuelpinedacabeza@gmail.com>
2022-04-01 13:42:30 +0200

    Required fixtures instead of using callbacks. Replaced admin token with testuser token.

--- e2e/cypress/integration/api-tests/podcast-episode-api.spec.js
+++ e2e/cypress/integration/api-tests/podcast-episode-api.spec.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
+const resourceTestData = require("../../fixtures/resourceAPITestData.json");
+const testuserData = require("../../fixtures/testuser.json");
+
 describe("Podacst Episode API 'GET' request", () => {
   before(() => {
     cy.deleteTestData();
-    cy.fixture("apiResourceData").then((testData) =>
-      cy.addResourceWithAPI("podcasts-episode", testData)
-    );
+    cy.addResourceWithAPI("podcasts-episode", resourceTestData);
   });
 
   it("should have status code 200", () => {
@@ -14,13 +15,11 @@ describe("Podacst Episode API 'GET' request", () => {
       url: "http://localhost:8000/api/podcast-episodes/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Token  ${Cypress.env("adminToken")}`,
-      }
-    }).then(
-      (response) => {
-        expect(response.status).to.eq(200);
-      }
-    );
+        Authorization: `Token  ${testuserData.token}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+    });
   });
 
   it("should return JSON", () => {
@@ -29,16 +28,14 @@ describe("Podacst Episode API 'GET' request", () => {
       url: "http://localhost:8000/api/podcast-episodes/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Token  ${Cypress.env("adminToken")}`,
-      }
-    }).then(
-      (response) => {
-        expect(response.headers).to.have.property(
-          "content-type",
-          "application/json"
-        );
-      }
-    );
+        Authorization: `Token  ${testuserData.token}`,
+      },
+    }).then((response) => {
+      expect(response.headers).to.have.property(
+        "content-type",
+        "application/json"
+      );
+    });
   });
 
   it("should have podcast episode fields", () => {
@@ -47,96 +44,86 @@ describe("Podacst Episode API 'GET' request", () => {
       url: "http://localhost:8000/api/podcast-episodes/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Token  ${Cypress.env("adminToken")}`,
-      }
-    }).then(
-      (response) => {
-        const firstMotivationalSpeech = response.body[0];
-        expect(firstMotivationalSpeech).to.have.property("id");
-        expect(firstMotivationalSpeech).to.have.property("title", "Test Title");
-        expect(firstMotivationalSpeech).to.have.property(
-          "author",
-          "Test Author"
-        );
-        expect(firstMotivationalSpeech).to.have.property("description");
-        expect(firstMotivationalSpeech).to.have.property("imageURL");
-        expect(firstMotivationalSpeech).to.have.property("spotify_episode_url");
-        expect(firstMotivationalSpeech).to.have.property("youtube_episode_url");
-        expect(firstMotivationalSpeech).to.have.property("avg_rating", 0);
-        expect(firstMotivationalSpeech).to.have.property("num_ratings", 0);
-      }
-    );
+        Authorization: `Token  ${testuserData.token}`,
+      },
+    }).then((response) => {
+      const firstPodcastEpisode = response.body[0];
+      expect(firstPodcastEpisode).to.have.property("id");
+      expect(firstPodcastEpisode).to.have.property("title", "Test Title");
+      expect(firstPodcastEpisode).to.have.property("author", "Test Author");
+      expect(firstPodcastEpisode).to.have.property("description");
+      expect(firstPodcastEpisode).to.have.property("imageURL");
+      expect(firstPodcastEpisode).to.have.property("from_podcast");
+      expect(firstPodcastEpisode).to.have.property("youtube_episode_url");
+      expect(firstPodcastEpisode).to.have.property("spotify_episode_url");
+      expect(firstPodcastEpisode).to.have.property("avg_rating", 0);
+      expect(firstPodcastEpisode).to.have.property("num_ratings", 0);
+    });
   });
 });
 
 describe("Podcast Episode API 'POST' request", () => {
   it("should have status code 201", () => {
-    cy.fixture("apiResourceData").then((testData) => {
-      cy.request({
-        method: "POST",
-        url: "http://localhost:8000/api/podcast-episodes/",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env("adminToken")}`,
-        },
-        body: JSON.stringify(testData),
-      }).then((response) => {
-        expect(response.status).to.eq(201);
-      });
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8000/api/podcast-episodes/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token  ${testuserData.token}`,
+      },
+      body: JSON.stringify(resourceTestData),
+    }).then((response) => {
+      expect(response.status).to.eq(201);
     });
   });
 
   it("should return JSON", () => {
-    cy.fixture("apiResourceData").then((testData) => {
-      cy.request({
-        method: "POST",
-        url: "http://localhost:8000/api/podcast-episodes/",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env("adminToken")}`,
-        },
-        body: JSON.stringify(testData),
-      }).then((response) => {
-        expect(response.headers).to.have.property(
-          "content-type",
-          "application/json"
-        );
-      });
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8000/api/podcast-episodes/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token  ${testuserData.token}`,
+      },
+      body: JSON.stringify(resourceTestData),
+    }).then((response) => {
+      expect(response.headers).to.have.property(
+        "content-type",
+        "application/json"
+      );
     });
   });
 
   it("should have podcast episode fields", () => {
-    cy.fixture("apiResourceData").then((testData) => {
-      cy.request({
-        method: "POST",
-        url: "http://localhost:8000/api/podcast-episodes/",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env("adminToken")}`,
-        },
-        body: JSON.stringify(testData),
-      }).then((response) => {
-        const podcastEpisode = response.body;
-        expect(podcastEpisode).to.have.property("id");
-        expect(podcastEpisode).to.have.property("title", testData.title);
-        expect(podcastEpisode).to.have.property("author", testData.author);
-        expect(podcastEpisode).to.have.property("description");
-        expect(podcastEpisode).to.have.property("imageURL");
-        expect(podcastEpisode).to.have.property(
-          "from_podcast",
-          testData.from_podcast
-        );
-        expect(podcastEpisode).to.have.property(
-          "youtube_episode_url",
-          testData.youtube_episode_url
-        );
-        expect(podcastEpisode).to.have.property(
-          "spotify_episode_url",
-          testData.spotify_episode_url
-        );
-        expect(podcastEpisode).to.have.property("avg_rating", 0);
-        expect(podcastEpisode).to.have.property("num_ratings", 0);
-      });
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8000/api/podcast-episodes/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token  ${testuserData.token}`,
+      },
+      body: JSON.stringify(resourceTestData),
+    }).then((response) => {
+      const podcastEpisode = response.body;
+      expect(podcastEpisode).to.have.property("id");
+      expect(podcastEpisode).to.have.property("title", resourceTestData.title);
+      expect(podcastEpisode).to.have.property("author", resourceTestData.author);
+      expect(podcastEpisode).to.have.property("description");
+      expect(podcastEpisode).to.have.property("imageURL");
+      expect(podcastEpisode).to.have.property(
+        "from_podcast",
+        resourceTestData.from_podcast
+      );
+      expect(podcastEpisode).to.have.property(
+        "youtube_episode_url",
+        resourceTestData.youtube_episode_url
+      );
+      expect(podcastEpisode).to.have.property(
+        "spotify_episode_url",
+        resourceTestData.spotify_episode_url
+      );
+      expect(podcastEpisode).to.have.property("avg_rating", 0);
+      expect(podcastEpisode).to.have.property("num_ratings", 0);
     });
   });
 });
