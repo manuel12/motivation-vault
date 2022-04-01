e62c64f1dbe710e5daf668f974a1c5dc599972a0
Manuel <manuelpinedacabeza@gmail.com>
2022-04-01 15:31:19 +0200

    Added loginAndCleanUp func. Updated classes to data attributes.

--- e2e/cypress/integration/ui-tests/page-not-found.spec.js
+++ e2e/cypress/integration/ui-tests/page-not-found.spec.js
@@ -2,35 +2,32 @@
 
 describe("404 Page not found", () => {
   beforeEach(() => {
-    cy.deleteTestData();
-    cy.loginWithAPI("testuser1", "testpass1");
-    cy.get(".App").should("be.visible");
+    cy.loginAndCleanUp();
+    cy.get("[data-test=app]").should("be.visible");
   });
-
-  // Add tests to check that models can be submitted without filling out optional fields.
 
   it("should show 404 page not found on a non-existant url", () => {
     cy.visit("/non-existant/url/");
-    cy.get(".not-found").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible");
   });
 
   it("should show 404 page not found on a non-existant url on books section", () => {
-    cy.visit("/books//non-existant/url/");
-    cy.get(".not-found").should("be.visible");
+    cy.visit("/books/non-existant/url/");
+    cy.get("[data-test=not-found]").should("be.visible");
   });
 
   it("should show 404 page not found on a non-existant url on podcasts section", () => {
-    cy.visit("/podcasts//non-existant/url/");
-    cy.get(".not-found").should("be.visible");
+    cy.visit("/podcasts/non-existant/url/");
+    cy.get("[data-test=not-found]").should("be.visible");
   });
 
   it("should show 404 page not found on a non-existant url on podcasts episode section", () => {
     cy.visit("/podcasts-episodes/non-existant/url/");
-    cy.get(".not-found").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible");
   });
 
   it("should show 404 page not found on a non-existant url on motivational speeches section", () => {
     cy.visit("/motivational-speeches/non-existant/url/");
-    cy.get(".not-found").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible");
   });
 });
