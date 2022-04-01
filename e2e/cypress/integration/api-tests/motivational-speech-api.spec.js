f0227bcf3453c51c88656b7611ee99c7ecfd9d71
Manuel <manuelpinedacabeza@gmail.com>
2022-04-01 13:43:05 +0200

    Required fixtures instead of using callbacks. Replaced admin token with testuser token.

--- e2e/cypress/integration/api-tests/motivational-speech-api.spec.js
+++ e2e/cypress/integration/api-tests/motivational-speech-api.spec.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
+const resourceTestData = require("../../fixtures/resourceAPITestData.json");
+const testuserData = require("../../fixtures/testuser.json");
+
 describe("Motivational Speech API 'GET' request", () => {
   before(() => {
     cy.deleteTestData();
-    cy.fixture("apiResourceData").then((testData) =>
-      cy.addResourceWithAPI("motivational-speech", testData)
-    );
+    cy.addResourceWithAPI("motivational-speech", resourceTestData);
   });
 
   it("should have status code 200", () => {
@@ -14,7 +15,7 @@ describe("Motivational Speech API 'GET' request", () => {
       url: "http://localhost:8000/api/motivational-speeches/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Token  ${Cypress.env("adminToken")}`,
+        Authorization: `Token  ${testuserData.token}`,
       },
     }).then((response) => {
       expect(response.status).to.eq(200);
@@ -27,7 +28,7 @@ describe("Motivational Speech API 'GET' request", () => {
       url: "http://localhost:8000/api/motivational-speeches/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Token  ${Cypress.env("adminToken")}`,
+        Authorization: `Token  ${testuserData.token}`,
       },
     }).then((response) => {
       expect(response.headers).to.have.property(
@@ -43,7 +44,7 @@ describe("Motivational Speech API 'GET' request", () => {
       url: "http://localhost:8000/api/motivational-speeches/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Token  ${Cypress.env("adminToken")}`,
+        Authorization: `Token  ${testuserData.token}`,
       },
     }).then((response) => {
       const firstMotivationalSpeech = response.body[0];
@@ -60,65 +61,59 @@ describe("Motivational Speech API 'GET' request", () => {
 });
 
 describe("Motivational Speech API 'POST' request", () => {
-    it("should have status code 201", () => {
-    cy.fixture("apiResourceData").then((testData) => {
-      cy.request({
-        method: "POST",
-        url: "http://localhost:8000/api/motivational-speeches/",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env("adminToken")}`,
-        },
-        body: JSON.stringify(testData),
-      }).then((response) => {
-        expect(response.status).to.eq(201);
-      });
+  it("should have status code 201", () => {
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8000/api/motivational-speeches/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token  ${testuserData.token}`,
+      },
+      body: JSON.stringify(resourceTestData),
+    }).then((response) => {
+      expect(response.status).to.eq(201);
     });
   });
 
   it("should return JSON", () => {
-    cy.fixture("apiResourceData").then((testData) => {
-      cy.request({
-        method: "POST",
-        url: "http://localhost:8000/api/motivational-speeches/",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env("adminToken")}`,
-        },
-        body: JSON.stringify(testData),
-      }).then((response) => {
-        expect(response.headers).to.have.property(
-          "content-type",
-          "application/json"
-        );
-      });
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8000/api/motivational-speeches/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token  ${testuserData.token}`,
+      },
+      body: JSON.stringify(resourceTestData),
+    }).then((response) => {
+      expect(response.headers).to.have.property(
+        "content-type",
+        "application/json"
+      );
     });
   });
 
   it("should have motivationalSpeech episode fields", () => {
-    cy.fixture("apiResourceData").then((testData) => {
-      cy.request({
-        method: "POST",
-        url: "http://localhost:8000/api/motivational-speeches/",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env("adminToken")}`,
-        },
-        body: JSON.stringify(testData),
-      }).then((response) => {
-        const motivationalSpeech = response.body;
-        expect(motivationalSpeech).to.have.property("id");
-        expect(motivationalSpeech).to.have.property("title", testData.title);
-        expect(motivationalSpeech).to.have.property("author", testData.author);
-        expect(motivationalSpeech).to.have.property("description");
-        expect(motivationalSpeech).to.have.property("imageURL");
-        expect(motivationalSpeech).to.have.property(
-          "youtube_url",
-          testData.youtube_url
-        );
-        expect(motivationalSpeech).to.have.property("avg_rating", 0);
-        expect(motivationalSpeech).to.have.property("num_ratings", 0);
-      });
+    cy.request({
+      method: "POST",
+      url: "http://localhost:8000/api/motivational-speeches/",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token  ${testuserData.token}`,
+      },
+      body: JSON.stringify(resourceTestData),
+    }).then((response) => {
+      const motivationalSpeech = response.body;
+      expect(motivationalSpeech).to.have.property("id");
+      expect(motivationalSpeech).to.have.property("title", resourceTestData.title);
+      expect(motivationalSpeech).to.have.property("author", resourceTestData.author);
+      expect(motivationalSpeech).to.have.property("description");
+      expect(motivationalSpeech).to.have.property("imageURL");
+      expect(motivationalSpeech).to.have.property(
+        "youtube_url",
+        resourceTestData.youtube_url
+      );
+      expect(motivationalSpeech).to.have.property("avg_rating", 0);
+      expect(motivationalSpeech).to.have.property("num_ratings", 0);
     });
   });
 });
