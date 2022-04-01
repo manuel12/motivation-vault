f2d98aa0d04472b344ec49d781a8c7211d1daa06
Manuel <manuelpinedacabeza@gmail.com>
2022-04-01 15:48:34 +0200

    Required fixtures, remove us of callbacks. Added loginAndCleanUp func. Changed classes for data attributes.

--- e2e/cypress/integration/ui-tests/add-podcast-resource.spec.js
+++ e2e/cypress/integration/ui-tests/add-podcast-resource.spec.js
@@ -1,51 +1,46 @@
 /// <reference types="cypress" />
 
+const resourceTestData = require("../../fixtures/resourceTestData.json");
+const resourceInvalidTestData = require("../../fixtures/invalidResourceTestData.json");
+
 describe("Add Podcast Resources", () => {
   beforeEach(() => {
-    cy.deleteTestData();
-    
-    cy.loginWithAPI("testuser1", "testpass1");
-    cy.get(".App").should("be.visible");
+    cy.loginAndCleanUp();
+    cy.get("[data-test=app]").should("be.visible");
   });
 
   it("should add a podcast resource", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("podcast", resourceData);
+    cy.addResourceWithUI("podcast", resourceTestData);
 
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author)
-        .and("contain", resourceData.description);
-    });
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author)
+      .and("contain", resourceTestData.description);
   });
 
   it("should display added podcast resource on podcast section", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("podcast", resourceData);
+    cy.addResourceWithUI("podcast", resourceTestData);
 
-      cy.visit("/podcasts/");
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author)
-        .and("contain", resourceData.description);
-    });
+    cy.visit("/podcasts/");
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author)
+      .and("contain", resourceTestData.description);
   });
 
   it("should add a podcast resource filling required fields only", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.addResourceWithUI("podcast", resourceData, true);
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author);
-    });
+    cy.addResourceWithUI("podcast", resourceTestData, true);
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author);
   });
 
   it("should NOT submit podcast form without filling required fields", () => {
     cy.visit("/add/");
-    cy.get(".add-container").should("be.visible");
+    cy.get("[data-test=add-container]").should("be.visible");
     cy.get("[data-test=select-resource-type]").select("Podcast");
 
     cy.get("[data-test=submit]").click();
@@ -55,56 +50,56 @@ describe("Add Podcast Resources", () => {
     cy.get("[data-test=title-input-error]").should("be.visible");
     cy.get("[data-test=author-input-error]").should("be.visible");
     cy.get("[data-test=website-url-input-error]").should("be.visible");
-    cy.get("[data-test=spotify-url-input-error]").should("be.visible");
-    cy.get("[data-test=youtube-url-input-error]").should("be.visible");
+    cy.get("[data-test=spotify-page-url-input-error]").should("be.visible");
+    cy.get("[data-test=youtube-page-url-input-error]").should("be.visible");
   });
 
   it("should NOT add a podcast resource with invalid data", () => {
-    cy.fixture("invalidResourceData").then((invalidData) => {
-      cy.addResourceWithUI("podcast", invalidData);
+    cy.addResourceWithUI("podcast", resourceInvalidTestData);
 
-      cy.url().should("contain", "add/");
-      
-      cy.get("[data-test=website-url-input-error]")
-        .should("be.visible")
-        .and("contain.text", "Website URL has to be a valid url!");
+    cy.url().should("contain", "add/");
 
-      cy.get("[data-test=spotify-url-input-error]")
-        .should("be.visible")
-        .and("contain.text", "Spotify URL has to be a valid url!");
+    cy.get("[data-test=website-url-input-error]")
+      .should("be.visible")
+      .and("contain.text", "Website URL has to be a valid url!");
 
-      cy.get("[data-test=youtube-url-input-error]")
-        .should("be.visible")
-        .and("contain.text", "Youtube URL has to be a valid url!");
-    });
+    cy.get("[data-test=spotify-page-url-input-error]")
+      .should("be.visible")
+      .and("contain.text", "Spotify URL has to be a valid url!");
+
+    cy.get("[data-test=youtube-page-url-input-error]")
+      .should("be.visible")
+      .and("contain.text", "Youtube URL has to be a valid url!");
   });
 
   it("should add a podcast resource by pressing ENTER when all fields are filled", () => {
-    cy.fixture("resourceData").then((resourceData) => {
-      cy.visit("/add/");
-      cy.get(".add-container").should("be.visible");
-      cy.get("[data-test=select-resource-type]").select("Podcast");
+    cy.visit("/add/");
+    cy.get("[data-test=add-container]").should("be.visible");
+    cy.get("[data-test=select-resource-type]").select("Podcast");
 
-      cy.get("[data-test=title-input]").type(resourceData.title);
-      cy.get("[data-test=author-input]").type(resourceData.author);
-      cy.get("[data-test=description-input]").type(resourceData.description);
+    cy.get("[data-test=title-input]").type(resourceTestData.title);
+    cy.get("[data-test=author-input]").type(resourceTestData.author);
+    cy.get("[data-test=description-input]").type(resourceTestData.description);
 
-      cy.get("[data-test=website-url-input]").type(resourceData.websiteUrl);
-      cy.get("[data-test=spotify-url-input]").type(resourceData.spotifyUrl);
-      cy.get("[data-test=youtube-url-input]").type(resourceData.youtubeUrl);
+    cy.get("[data-test=website-url-input]").type(resourceTestData.websiteUrl);
+    cy.get("[data-test=spotify-page-url-input]").type(
+      resourceTestData.spotifyUrl
+    );
+    cy.get("[data-test=youtube-page-url-input]").type(
+      resourceTestData.youtubeUrl
+    );
 
-      cy.get("[data-test=value-one-input]").type(resourceData.valueOne);
-      cy.get("[data-test=value-two-input]").type(resourceData.valueTwo);
-      cy.get("[data-test=value-three-input]").type(
-        `${resourceData.valueThree}{enter}`
-      );
+    cy.get("[data-test=value-one-input]").type(resourceTestData.valueOne);
+    cy.get("[data-test=value-two-input]").type(resourceTestData.valueTwo);
+    cy.get("[data-test=value-three-input]").type(
+      `${resourceTestData.valueThree}{enter}`
+    );
 
-      cy.get("[data-test=post-container]")
-        .first()
-        .should("contain", resourceData.title)
-        .and("contain", resourceData.author)
-        .and("contain", resourceData.description);
-    });
+    cy.get("[data-test=post-container]")
+      .first()
+      .should("contain", resourceTestData.title)
+      .and("contain", resourceTestData.author)
+      .and("contain", resourceTestData.description);
   });
 
   after(() => {
