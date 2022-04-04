87dad5720f10fc95dc6bb5596ea0a9f86d4c2a10
Manuel <manuelpinedacabeza@gmail.com>
2022-04-04 19:44:48 +0200

    Updated let to const.

--- front-end/src/components/Podcastpage.js
+++ front-end/src/components/Podcastpage.js
@@ -5,7 +5,7 @@ import useToken from './useToken';
 
 function PodcastPage() {
   const {token} = useToken();
-  let [resources, setResources] = useState([]);
+  const [resources, setResources] = useState([]);
 
   useEffect(() => {
     API.fetchResource({
