38d2c81d46c50893536ac57f8619332ca8d9be60
Manuel <manuelpinedacabeza@gmail.com>
2022-04-04 19:50:16 +0200

    Added states for usernameError & passwordError. Added data-attributes to heading. Updated evt to e. Added data-attributes to username and password inputs and submit-button.

--- front-end/src/components/Auth.js
+++ front-end/src/components/Auth.js
@@ -10,13 +10,22 @@ function Auth({ setToken }) {
   const [password, setPassword] = useState("");
   const [isLoginView, setLoginView] = useState(true);
 
+  const [usernameError, setUsernameError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   const loginClicked = () => {
+    if (username === "") setUsernameError(true);
+    if (password === "") setPasswordError(true);
+
     API.loginUser({ username, password })
       .then((resp) => setToken(resp.token))
       .catch((err) => console.log(err));
   };
 
   const registerClicked = () => {
+    if (username === "") setUsernameError(true);
+    if (password === "") setPasswordError(true);
+
     API.registerUser({ username, password })
       .then(() => loginClicked())
       .catch((error) => console.log(error));
@@ -36,39 +45,44 @@ function Auth({ setToken }) {
   return (
     <div>
       <div className="header">
-        {isLoginView ? <h1>Login</h1> : <h1>Register</h1>}
+        {isLoginView ? <h1 data-test="heading">Login</h1> : <h1>Register</h1>}
       </div>
 
       <div className="login-container">
         <form onSubmit={submitHandler}>
-          <FontAwesomeIcon icon={faEnvelope} className="icon"/>
-          <label htmlFor="username">Username</label>
-          <br />
+          {usernameError && (
+            <div class="username-error" data-test="username-error">
+              <FontAwesomeIcon icon={faEnvelope} className="icon" />
+              <label htmlFor="username">You need to provide a username.</label>
+            </div>
+          )}
           <input
             ref={searchInput}
             id="username"
             type="text"
             placeholder="Enter your username"
             value={username}
-            onChange={(evt) => setUsername(evt.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
+            data-test="username"
           />
-          <br />
-          <FontAwesomeIcon icon={faLock} className="icon"/>
-          <label htmlFor="password">Password</label>
-          <br />
+          {passwordError && (
+            <div div class="password-error" data-test="password-error">
+              <FontAwesomeIcon icon={faLock} className="icon" />
+              <label htmlFor="password">You need to provide a password.</label>
+            </div>
+          )}
           <input
             id="password"
             type="password"
             placeholder="Enter your password"
             value={password}
-            onChange={(evt) => setPassword(evt.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
+            data-test="password"
           ></input>
           <br />
-
-          <button id="submitButton" type="submit">
+          <button id="submitButton" type="submit" data-test="submit-button">
             {isLoginView ? "Login" : "Register"}
           </button>
-
           {isLoginView ? (
             <p onClick={() => setLoginView(false)}>
               You don't have an account? Register here!
