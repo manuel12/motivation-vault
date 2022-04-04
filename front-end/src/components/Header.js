079e81b03267e3e47e55121a3cd9100d38e586c9
Manuel <manuelpinedacabeza@gmail.com>
2022-04-04 19:42:53 +0200

    Added data-attribute to header

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -3,7 +3,7 @@ import "../css/Header.css";
 function Header(props) {
   return (
     <div>
-      <header className="header">
+      <header className="header" data-test="heading">
         <a className="header-link" href="/">
           <h1>Resource API Project</h1>
         </a>
