ccb74c536b9b2ec82d097739d7de757398101c70
Manuel <manuelpinedacabeza@gmail.com>
2022-04-05 19:23:27 +0200

    Imported css module, used imported classes for styling.

--- front-end/src/components/Comment.js
+++ front-end/src/components/Comment.js
@@ -1,8 +1,8 @@
-import "../css/Comment.css";
+import classes from "../css/Comment.module.css";
 
 function Comment(props) {
   return (
-    <div className="comment-container" data-test="comment-container">
+    <div className={classes["comment-container"]} data-test="comment-container">
       <div>
         <strong>{props.user}</strong> - {props.date}
       </div>
