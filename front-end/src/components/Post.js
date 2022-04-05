e03af0c7151543889859bd68c2b93351f2ac048a
Manuel <manuelpinedacabeza@gmail.com>
2022-04-05 19:21:35 +0200

    Imported css module, used imported classes for styling.

--- front-end/src/components/Post.js
+++ front-end/src/components/Post.js
@@ -1,23 +1,30 @@
 import RatingSection from "./RatingSection";
-import "../css/Post.css";
+import classes from "../css/Post.module.css";
 
 function Post(props) {
   return (
-    <div className="post-container" data-test="post-container">
-      <a href={`http://localhost:3000/${props.id}/`} className="container-link">
-        <div className="image-container">
-          <img src={props.imageURL} className="post-image" alt=""></img>
+    <div className={classes["post-container"]} data-test="post-container">
+      <a
+        href={`http://localhost:3000/${props.id}/`}
+        className={classes["container-link"]}
+      >
+        <div className={classes["image-container"]}>
+          <img
+            src={props.imageURL}
+            className={classes["post-image"]}
+            alt=""
+          ></img>
         </div>
-        <div className="text-container">
-          <h3>{props.title}</h3>
-          <div className="author-text-container">
-            by <h5 className="author-name">{props.author}</h5>
+        <div className={classes["text-container"]}>
+          <h3 classNam>{classes["h3"]}{props.title}</h3>
+          <div className={classes["author-text-container"]}>
+            by <h5 className={classes["author-name"]}>{props.author}</h5>
           </div>
           <RatingSection
             avgRating={props.avgRating}
             numRatings={props.numRatings}
           />
-          <p className="description-container">
+          <p className={classes["description-container"]}>
             {props.description && props.description.length > 300
               ? `${props.description.substring(0, 300)}...`
               : props.description}
