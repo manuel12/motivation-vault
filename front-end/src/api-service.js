96c7b60ea013c428f5b383417320dfd3f2edf972
Manuel <manuelpinedacabeza@gmail.com>
2022-04-07 14:15:04 +0200

    Added _fetch function. Added  fetchAllResources, fetchAllResourcesOfType functions. Refactored fetchResource.

--- front-end/src/api-service.js
+++ front-end/src/api-service.js
@@ -1,58 +1,67 @@
+const _fetch = (url, token) => {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+  });
+};
+
 export class API {
   static loginUser(body) {
     return fetch(`http://127.0.0.1:8000/auth/`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     })
-    .then(resp => resp.json())
-    .catch( error => console.log(error))
+      .then((resp) => resp.json())
+      .catch((error) => console.log(error));
   }
 
   static registerUser(body) {
     return fetch(`http://127.0.0.1:8000/api/users/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
-    }).then( resp => resp.json())
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }).then((resp) => resp.json());
   }
 
-  static fetchResource(options) {
-    let url
-    if(options.id) {
-      url = `http://127.0.0.1:8000/api/${options.id}/`
-    } else {
-      url =  options.resource === 'home' ?
-        `http://127.0.0.1:8000/api/` :
-        `http://127.0.0.1:8000/api/${options.resource}/` 
-    }
-    
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization':  `Token ${options.token}`
-      }
-    })
-    .then( resp => resp.json())
-    .then( resp => options.setResourceFunc(resp)) 
-    .catch( error => console.log(error))
+  static fetchAllResources(setResourceFunc) {
+    return _fetch("http://127.0.0.1:8000/api/")
+      .then((resp) => resp.json())
+      .then((resp) => setResourceFunc(resp))
+      .catch((error) => console.log(error));
+  }
+
+  static fetchAllResourcesOfType(resourceType, token, setResourceFunc) {
+    let url = `http://127.0.0.1:8000/api/${resourceType}/`;
+    _fetch(url, token)
+      .then((resp) => resp.json())
+      .then((resp) => setResourceFunc(resp))
+      .catch((error) => console.log(error));
+  }
+
+  static fetchResource(id, token, setResourceFunc) {
+    let url = `http://127.0.0.1:8000/api/${id}/`;
+    _fetch(url, token)
+      .then((resp) => resp.json())
+      .then((resp) => setResourceFunc(resp))
+      .catch((error) => console.log(error));
   }
 
   static createResource(options) {
     return fetch(`http://127.0.0.1:8000/api/`, {
-      method: 'POST',
-      headers:
-      {
-        'Content-Type': 'application/json',
-        'Authorization': `Token ${options.token}`
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Token ${options.token}`,
       },
-      body: JSON.stringify(options.body)
-    }).then(resp => resp.json())
+      body: JSON.stringify(options.body),
+    }).then((resp) => resp.json());
   }
 }
-
