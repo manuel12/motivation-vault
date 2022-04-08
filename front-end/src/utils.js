2f543594de4c1f5346e8db97d67c7038e87c092d
Manuel <manuelpinedacabeza@gmail.com>
2022-04-08 12:03:27 +0200

    Renamed isValidIsbn to isValidISBN. Added validator class.

--- front-end/src/utils.js
+++ front-end/src/utils.js
@@ -1,4 +1,4 @@
-export function isValidIsbn(str) {
+export function isValidISBN(str) {
   /**
    * Checks ISBN validity on add book resource forms.
    * Cortesy of derek-kurth: https://stackoverflow.com/a/23161438
@@ -75,4 +75,178 @@ export function getEmbedYoutubeUrl(resource) {
     embedYoutubeUrl = fallback;
   }
   return embedYoutubeUrl;
+}
+
+export class Validator {
+  constructor() {
+    this.validationPassed = true;
+    this.errors = new Set();
+  }
+
+  validate() {
+    if (this.validationPassed) {
+      console.log("Validation passed!");
+      return true;
+    } else {
+      console.log("Validation failed!");
+      console.log("Errors: ");
+      for (let error of this.errors) {
+        console.log(error);
+      }
+      return false;
+    }
+  }
+
+  registerError(error) {
+    this.validationPassed = false;
+    this.errors.add(error);
+  }
+
+  registerTitle(title, setTitleError) {
+    const titleEmptyError = "Title cannot be empty!";
+    if (!title) {
+      setTitleError(titleEmptyError);
+      this.registerError(titleEmptyError);
+    } else {
+      setTitleError("");
+    }
+  }
+
+  registerAuthor(author, setAuthorError) {
+    const authorEmptyError = "Author cannot be empty!";
+    if (!author) {
+      this.registerError(authorEmptyError);
+      setAuthorError(authorEmptyError);
+    } else {
+      setAuthorError("");
+    }
+  }
+
+  registerSubtitle(subtitle, setSubtitleError) {
+    const subtitleEmptyError = "Subtitle cannot be empty!";
+    if (!subtitle) {
+      setSubtitleError(subtitleEmptyError);
+      this.registerError(subtitleEmptyError);
+    } else {
+      setSubtitleError("");
+    }
+  }
+
+  registerISBN(isbn, setISBNError) {
+    const ISBNEmptyError = "ISBN cannot be empty!";
+    const ISBNInvalidError = "ISBN has to be a 13 digits!";
+
+    if (!isbn) {
+      setISBNError(ISBNEmptyError);
+      this.registerError(ISBNEmptyError);
+    } else if (!isValidISBN(isbn)) {
+      setISBNError(ISBNInvalidError);
+      this.registerError(ISBNInvalidError);
+    } else {
+      setISBNError("");
+    }
+  }
+
+  registerWebsiteUrl(websiteUrl, setWebsiteUrlError) {
+    const websiteUrlEmptyError = "Website URL cannot be empty!";
+    const websiteUrlInvalidError = "Website URL has to be a valid url!";
+
+    if (!websiteUrl) {
+      setWebsiteUrlError(websiteUrlEmptyError);
+      this.registerError(websiteUrlEmptyError);
+    } else if (!isValidUrl(websiteUrl)) {
+      setWebsiteUrlError(websiteUrlInvalidError);
+      this.registerError(websiteUrlInvalidError);
+    } else {
+      setWebsiteUrlError("");
+    }
+  }
+
+  registerYoutubePageUrl(youtubePageUrl, setYoutubePageUrlError) {
+    const youtubePageUrlEmptyError = "Youtube URL cannot be empty!";
+    const youtubePageUrlInvalidError = "Youtube URL has to be a valid url!";
+
+    if (!youtubePageUrl) {
+      setYoutubePageUrlError(youtubePageUrlEmptyError);
+      this.registerError(youtubePageUrlEmptyError);
+    } else if (!isValidUrl(youtubePageUrl)) {
+      setYoutubePageUrlError(youtubePageUrlInvalidError);
+      this.registerError(youtubePageUrlInvalidError);
+    } else {
+      setYoutubePageUrlError("");
+    }
+  }
+
+  registerSpotifyPageUrl(spotifyPageUrl, setSpotifyPageUrlError) {
+    const spotifyPageUrlEmptyError = "Spotify URL cannot be empty!";
+    const spotifyPageUrlInvalidError = "Spotify URL has to be a valid url!";
+
+    if (!spotifyPageUrl) {
+      setSpotifyPageUrlError(spotifyPageUrlEmptyError);
+      this.registerError(spotifyPageUrlEmptyError);
+    } else if (!isValidUrl(spotifyPageUrl)) {
+      setSpotifyPageUrlError(spotifyPageUrlInvalidError);
+      this.registerError(spotifyPageUrlInvalidError);
+    } else {
+      setSpotifyPageUrlError("");
+    }
+  }
+
+  registerPodcast(podcast, setPodcastError) {
+    const podcastEmptyError = "Podcast cannot be empty!";
+    if (!podcast) {
+      setPodcastError(podcastEmptyError);
+      this.registerError(podcastEmptyError);
+    } else {
+      setPodcastError("");
+    }
+  }
+
+  registerYoutubeEpisodeUrl(youtubeEpisodeUrl, setYoutubeEpisodeUrlError) {
+    const youtubeEpisodeUrlEmptyError = "Youtube episode URL cannot be empty!";
+    const youtubeEpisodeUrlInvalidError =
+      "Youtube episode URL has to be a valid url!";
+
+    if (!youtubeEpisodeUrl) {
+      setYoutubeEpisodeUrlError(youtubeEpisodeUrlEmptyError);
+      this.registerError(youtubeEpisodeUrlEmptyError);
+    } else if (!isValidUrl(youtubeEpisodeUrl)) {
+      setYoutubeEpisodeUrlError(youtubeEpisodeUrlInvalidError);
+      this.registerError(youtubeEpisodeUrlInvalidError);
+    } else {
+      setYoutubeEpisodeUrlError("");
+    }
+  }
+
+  registerSpotifyEpisodeUrl(spotifyEpisodeUrl, setSpotifyEpisodeUrlError) {
+    const spotifyEpisodeUrlEmptyError = "Spotify episode URL cannot be empty!";
+    const spotifyEpisodeUrlInvalidError =
+      "Spotify episode URL has to be a valid url!";
+
+    if (!spotifyEpisodeUrl) {
+      setSpotifyEpisodeUrlError(spotifyEpisodeUrlEmptyError);
+      this.registerError(spotifyEpisodeUrlEmptyError);
+    } else if (!isValidUrl(spotifyEpisodeUrl)) {
+      setSpotifyEpisodeUrlError(spotifyEpisodeUrlInvalidError);
+      this.registerError(spotifyEpisodeUrlInvalidError);
+    } else {
+      setSpotifyEpisodeUrlError("");
+    }
+  }
+
+  registerYoutubeUrl(youtubeUrl, setYoutubeUrlError) {
+    const youtubeUrlEmptyError = "Youtube URL cannot be empty!";
+    const youtubeUrlInvalidError = "Youtube URL has to be a valid URL!";
+
+    if (!youtubeUrl) {
+      setYoutubeUrlError(youtubeUrlEmptyError);
+      this.registerError(youtubeUrlEmptyError);
+    } else if (!isValidUrl(youtubeUrl)) {
+      setYoutubeUrlError(youtubeUrlInvalidError);
+      this.registerError(youtubeUrlInvalidError);
+    } else {
+      setYoutubeUrlError("");
+      return true;
+    }
+  }
 }
