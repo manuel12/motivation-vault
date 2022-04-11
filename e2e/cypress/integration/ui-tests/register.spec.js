5f234617bc19ee115b83440bbacad2485c52b757
Manuel <manuelpinedacabeza@gmail.com>
2022-04-11 23:24:16 +0200

    Added navbar and registration tests.

---
+++ e2e/cypress/integration/ui-tests/register.spec.js
@@ -0,0 +1,71 @@
+/// <reference types="cypress" />
+
+const testuserData = require("../../fixtures/registerUser.json");
+
+const testuser2Data = require("../../fixtures/testuser.json");
+
+describe("Register", () => {
+  beforeEach(() => {
+    cy.deleteTestData();
+    cy.visit("/");
+    cy.get("[data-test=register-link]").click();
+  });
+
+  it("should register with valid username and password", () => {
+    cy.get("[data-test=username]").type(testuserData.username);
+    cy.get("[data-test=password]").type(testuserData.password);
+    cy.get("[data-test=submit-button]").click();
+
+    cy.get("[data-test=heading]").should(
+      "contain.text",
+      "Resource API Project"
+    );
+    cy.get("[data-test=nav-list]").should("be.visible");
+    cy.get("[data-test=homepage]").should("be.visible");
+  });
+
+  it("should NOT register with existing username and password", () => {
+    cy.get("[data-test=username]").type(testuser2Data.username);
+    cy.get("[data-test=password]").type(testuser2Data.password);
+    cy.get("[data-test=submit-button]").click();
+
+    cy.get("[data-test=heading]").should("contain.text", "Register");
+    cy.get("[data-test=nav-list]").should("not.exist");
+    cy.get("[data-test=homepage]").should("not.exist");
+  });
+
+  it("should NOT register with empty username and password fields", () => {
+    cy.get("[data-test=submit-button]").click();
+
+    cy.get("[data-test=heading]").should("contain.text", "Register");
+    cy.get("[data-test=nav-list]").should("not.exist");
+    cy.get("[data-test=homepage]").should("not.exist");
+  });
+
+  it.skip("should have focus on username input", () => {
+    cy.log(cy.focused());
+    cy.focused().should("have.id", "username");
+  });
+
+  it("should show error message when leaving username empty", () => {
+    cy.get("[data-test=password]").type(testuserData.password);
+    cy.get("[data-test=submit-button]").click();
+
+    cy.get("[data-test=username-error]")
+      .should("be.visible")
+      .and("contain.text", "You need to provide a username.");
+  });
+
+  it("should show error message when leaving password empty", () => {
+    cy.get("[data-test=username]").type(testuserData.username);
+    cy.get("[data-test=submit-button]").click();
+
+    cy.get("[data-test=password-error]")
+      .should("be.visible")
+      .and("contain.text", "You need to provide a password.");
+  });
+
+  afterEach(() => {
+    cy.deleteTestData();
+  });
+});
