8114dfe362e4d65c421a34df3f14e8168df8f132
Manuel <manuelpinedacabeza@gmail.com>
2022-04-12 11:51:06 +0200

    Added 4 space indentation.

--- api_project/settings.py
+++ api_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #3rd party
+    # 3rd party
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
@@ -142,20 +142,20 @@ STATIC_URL = '/static/'
 # Heroku settings
 
 if os.getcwd() == '/app':
-  import dj_database_url
-  DATABASES = {
-    'default' :dj_database_url.config(default='postgres://localhost')
-  }
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
 
-  # Honor the 'X-Forwarded-Proto' header for request.is_secure().
-  SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-  # Allow all host headers.
-  ALLOWED_HOSTS = ['*']
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
 
-  # Static asset configuration
-  BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-  STATIC_ROOT = 'staticfiles'
-  STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-  )
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
