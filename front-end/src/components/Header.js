d3b06edab49138d8cd29ba67aa01cff4816362a8
Manuel <manuelpinedacabeza@gmail.com>
2022-04-13 15:47:20 +0200

    Removed unnecesary class names.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -3,7 +3,7 @@ import classes from "../css/Header.module.css";
 function Header(props) {
   return (
     <div>
-      <header className={classes["header"]} data-test="heading">
+      <header data-test="heading">
         <a className={classes["header-link"]} href="/">
           <h1>Resource API Project</h1>
         </a>
