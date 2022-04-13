a15863960ac7d40ad904ca1b489055aff381bb21
Manuel <manuelpinedacabeza@gmail.com>
2022-04-13 15:57:04 +0200

    Removed hardcoded fetch function. Removed getDetails function. Imported and used API instead. Added fa-star class to icons.

--- front-end/src/components/RatingSection.js
+++ front-end/src/components/RatingSection.js
@@ -4,6 +4,7 @@ import { faStar, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 import useToken from "./useToken";
 
 import classes from "../css/RatingSection.module.css";
+import { API } from "../api-service";
 
 function RatingSection(props) {
   const { token } = useToken();
@@ -31,34 +32,10 @@ function RatingSection(props) {
       stars: rating,
     };
 
-    fetch(`http://127.0.0.1:8000/api/ratings/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${token}`,
-      },
-      body: JSON.stringify(newRating),
-    })
-      .then((resp) => resp.json())
-      .then((resp) => {
-        getDetails();
-      })
-      .catch((error) => console.warn(error));
-
-    setShowRatingInput(false);
-  };
-
-  const getDetails = () => {
-    fetch(`http://127.0.0.1:8000/api/${props.resourceId}/`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token ${token}`,
-      },
-    })
-      .then((resp) => resp.json())
-      .then((resp) => props.updateResource(resp))
-      .catch((error) => console.log(error));
+    API.postRating(token, newRating, () => {
+      API.fetchResource(props.resourceId, token, props.updateResource);
+      setShowRatingInput(false);
+    });
   };
 
   let avgRating = props.avgRating;
@@ -68,27 +45,37 @@ function RatingSection(props) {
     <div className={classes["ratings-container"]} data-test="ratings-container">
       <FontAwesomeIcon
         icon={faStar}
-        className={avgRating > 0 ? classes["orange"] : classes["no-display"]}
+        className={`${
+          avgRating > 0 ? classes["orange"] : classes["no-display"]
+        } ${classes["fa-star"]}`}
         data-test="star-icon-1"
       />
       <FontAwesomeIcon
         icon={faStar}
-        className={avgRating > 1 ? classes["orange"] : classes["no-display"]}
+        className={`${
+          avgRating > 1 ? classes["orange"] : classes["no-display"]
+        } ${classes["fa-star"]}`}
         data-test="star-icon-2"
       />
       <FontAwesomeIcon
         icon={faStar}
-        className={avgRating > 2 ? classes["orange"] : classes["no-display"]}
+        className={`${
+          avgRating > 2 ? classes["orange"] : classes["no-display"]
+        } ${classes["fa-star"]}`}
         data-test="star-icon-3"
       />
       <FontAwesomeIcon
         icon={faStar}
-        className={avgRating > 3 ? classes["orange"] : classes["no-display"]}
+        className={`${
+          avgRating > 3 ? classes["orange"] : classes["no-display"]
+        } ${classes["fa-star"]}`}
         data-test="star-icon-4"
       />
       <FontAwesomeIcon
         icon={faStar}
-        className={avgRating > 4 ? classes["orange"] : classes["no-display"]}
+        className={`${
+          avgRating > 4 ? classes["orange"] : classes["no-display"]
+        } ${classes["fa-star"]}`}
         data-test="star-icon-5"
       />
 
