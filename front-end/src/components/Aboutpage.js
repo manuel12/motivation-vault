f7edc5981ee3209699674ebe7e2ed438823f1142
Manuel <manuelpinedacabeza@gmail.com>
2022-04-15 17:52:21 +0200

    Updated component to arrow func. Updated text.

--- front-end/src/components/Aboutpage.js
+++ front-end/src/components/Aboutpage.js
@@ -1,23 +1,23 @@
 import classes from "../css/Aboutpage.module.css";
 
-function AboutPage() {
+const AboutPage = () => {
   return (
-    <div className={classes['about-page-container']}>
+    <div className={classes["about-page-container"]}>
       <h2>About</h2>
       <hr></hr>
       <p>
-        The idea for this project came during the Covid pandemic of 2020-2021
-        and onwards. <br />
-        <br />
-        Seeing how so much negative media was being broadcasted on the press and
-        how little of any positive, inspiring and motivating media resources
-        were being aired during the same period of time I decided to make a
-        compilation of several media resources that I've read, listened to, and
-        watched during the same period of time that to me offer a more upbeat,
-        positive, inspiring and motivating content.
+        This website is compilation of
+        different resources thet I've been either reading, listening or watching for the
+        last 2 years that offer an upbeat and positive perspective with inspirational and
+        motivational content.
+        <br></br>
+        <br></br>
+        It was born out of seeing how much negative media was being broadcast on the press and
+        how little of any positive, inspiring and motivational stories
+        were being aired during the same period of time.
       </p>
     </div>
   );
-}
+};
 
 export default AboutPage;
