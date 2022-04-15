008b12eb16eb7284bef73c3d81adf43696e4045e
Manuel <manuelpinedacabeza@gmail.com>
2022-04-15 17:55:29 +0200

    Updated component to arrow func. Updated App name.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,13 +1,13 @@
 import classes from "../css/Header.module.css";
 
-function Header(props) {
+const Header = (props) => {
   return (
     <div>
       <header data-test="heading">
         <a className={classes["header-link"]} href="/">
-          <h1>Resource API Project</h1>
+          <h1>Motivation Vault</h1>
         </a>
-      </header>
+      </header> 
     </div>
   );
 }
