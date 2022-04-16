ada3f643c40da82b4266ede76b16785659f3d02d
Manuel <manuelpinedacabeza@gmail.com>
2022-04-16 18:44:23 +0200

    Imported obain_auth_token from views.

--- api_project/urls.py
+++ api_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from api.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
