da4d05b0152e238616dd8c35b67c03ef842a9745
Manuel <manuelpinedacabeza@gmail.com>
2022-04-18 14:10:01 +0200

    Updated app name on header.

--- e2e/cypress/integration/ui-tests/logout.spec.js
+++ e2e/cypress/integration/ui-tests/logout.spec.js
@@ -7,7 +7,7 @@ describe("Logout", () => {
   it("should logout by navigation bar", () => {
     cy.get("[data-test=logout-link]").click();
 
-    cy.get("[data-test=heading]").should("contain.text", "Login");
+    cy.get("[data-test=heading]").should("contain.text", "Motivation Vault");
     cy.get("[data-test=nav-list]").should("not.exist");
     cy.get("[data-test=homepage]").should("not.exist");
   });
