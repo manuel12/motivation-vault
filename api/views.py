25e29ac1a2ba4b7b00e211113bf5ce23bcc2d09d
Manuel <manuelpinedacabeza@gmail.com>
2022-04-19 17:49:18 +0200

    Reformatted code. Added TokenAuthentication to ResourceList and ResourceDetail. Added IsAuthenticated to ResourceList and ResourceDetail. Removed hardcoded authentication_classes and permission_classes from sub-classes.

--- api/views.py
+++ api/views.py
@@ -16,11 +16,12 @@ from resources import models
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request):
         serializer = self.serializer_class(
-          data=request.data, context={'request': request})
-        if(serializer.is_valid(raise_exception=False)):
-            user = serializer.validated_data['user']
+            data=request.data, context={"request": request}
+        )
+        if serializer.is_valid(raise_exception=False):
+            user = serializer.validated_data["user"]
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
+            return Response({"token": token.key})
         else:
             errors = {}
             error_types = ["username", "password", "non_field_errors"]
@@ -28,8 +29,7 @@ class CustomObtainAuthToken(ObtainAuthToken):
                 if error_type in serializer.errors:
                     errors[error_type] = str(serializer.errors[error_type][0])
             print(errors)
-            return Response({'errors': errors},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response({"errors": errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 obtain_auth_token = CustomObtainAuthToken.as_view()
@@ -38,7 +38,7 @@ obtain_auth_token = CustomObtainAuthToken.as_view()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
 
 
 class ResourceView(APIView):
@@ -56,14 +56,15 @@ class ResourceList(ResourceView):
     """
     List all resources, or create a new book.
     """
-    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def _post_error(self):
         response = {
-            'message':
-                'You cannot create from the Resource class directly. '
-                'You must create an instance of a subclass like '
-                'Book, Podcast, PodcastEpisode or Motivational Speech.'}
+            "message": "You cannot create from the Resource class directly. "
+            "You must create an instance of a subclass like "
+            "Book, Podcast, PodcastEpisode or Motivational Speech."
+        }
         return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
     def get(self, request):
@@ -76,7 +77,7 @@ class ResourceList(ResourceView):
         resource_name = self.get_resource_class().__name__
 
         # Do not allow any resource creation to occur from the Resource class.
-        if(resource_name == "Resource"):
+        if resource_name == "Resource":
             return self._post_error()
 
         serializer_class = self.get_serializer_class()
@@ -91,7 +92,8 @@ class ResourceDetail(ResourceView):
     """
     Retrieve a resource instance.
     """
-    permission_classes = (AllowAny,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self, pk):
         resource_class = self.get_resource_class()
@@ -111,8 +113,6 @@ class BookList(ResourceList):
     """
     List all books, or create a new book.
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     resource_class = models.Book
     serializer_class = serializers.BookSerializer
 
@@ -121,6 +121,7 @@ class BookDetail(ResourceDetail):
     """
     Retrieve a book instance.
     """
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     resource_class = models.Book
@@ -131,8 +132,7 @@ class PodcastList(ResourceList):
     """
     List all podcasts, or create a new podcast.
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
     resource_class = models.Podcast
     serializer_class = serializers.PodcastSerializer
 
@@ -141,6 +141,7 @@ class PodcastDetail(ResourceDetail):
     """
     Retrieve a podcast instance.
     """
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     resource_class = models.Podcast
@@ -151,8 +152,7 @@ class PodcastEpisodeList(ResourceList):
     """
     List all podcasts episodes, or create a new podcast episode.
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
     resource_class = models.PodcastEpisode
     serializer_class = serializers.PodcastEpisodeSerializer
 
@@ -161,6 +161,7 @@ class PodcastEpisodeDetail(ResourceDetail):
     """
     Retrieve a podcast episode instance.
     """
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     resource_class = models.PodcastEpisode
@@ -171,8 +172,7 @@ class MotivationalSpeechList(ResourceList):
     """
     List all motivational speeches, or create a new motivational speech.
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+
     resource_class = models.MotivationalSpeech
     serializer_class = serializers.MotivationalSpeechSerializer
 
@@ -181,6 +181,7 @@ class MotivationalSpeechDetail(ResourceDetail):
     """
     Retrieve a motivational speech instance.
     """
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     resource_class = models.MotivationalSpeech
@@ -191,6 +192,7 @@ class CommentList(APIView):
     """
     List all comments, create a new comment, or delete all comments.
     """
+
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
@@ -201,9 +203,9 @@ class CommentList(APIView):
 
     def post(self, request):
         comment_data = {
-            'user': request.user.pk,
-            'resource': request.data['resource'],
-            'text': request.data['text'],
+            "user": request.user.pk,
+            "resource": request.data["resource"],
+            "text": request.data["text"],
         }
         serializer = serializers.CommentSerializer(data=comment_data)
         if serializer.is_valid():
@@ -214,14 +216,15 @@ class CommentList(APIView):
     def delete(self, request):
         comments = models.Comment.objects.all()
         comments.delete()
-        return Response('Comments deleted!', status=status.HTTP_204_NO_CONTENT)
+        return Response("Comments deleted!", status=status.HTTP_204_NO_CONTENT)
 
 
 class RatingList(APIView):
     """
     List all ratings, or create a new rating.
     """
-    authentication_classes = (TokenAuthentication, )
+
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
@@ -236,10 +239,10 @@ class RatingList(APIView):
 
         If no stars are provided in request send a message.
         """
-        if 'stars' in request.data:
-            stars = int(request.data['stars'])
+        if "stars" in request.data:
+            stars = int(request.data["stars"])
             user = User.objects.get(pk=request.user.pk)
-            resource = models.Resource.objects.get(pk=request.data['resource'])
+            resource = models.Resource.objects.get(pk=request.data["resource"])
 
             try:
                 rating = models.Rating.objects.get(
@@ -249,35 +252,38 @@ class RatingList(APIView):
                 rating.save()
 
                 serializer = serializers.RatingSerializer(rating, many=False)
-                response = {'message': 'Rating updated',
-                            'result': serializer.data}
+                response = {"message": "Rating updated",
+                            "result": serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
             except models.Rating.DoesNotExist:
                 rating = models.Rating.objects.create(
-                    user=user, resource=resource, stars=stars)
+                    user=user, resource=resource, stars=stars
+                )
                 rating.clean_fields()
 
                 serializer = serializers.RatingSerializer(rating, many=False)
-                response = {'message': 'Rating created',
-                            'result': serializer.data}
+                response = {"message": "Rating created",
+                            "result": serializer.data}
                 return Response(response, status=status.HTTP_201_CREATED)
         else:
-            response = {'message': 'You need to provide stars',
-                        'data': request.data}
+            response = {"message": "You need to provide stars",
+                        "data": request.data}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([AllowAny])
 def delete_test_data(request):
     """
     Delete all resources, comments and ratings created by tests.
     """
     test_resources = models.Resource.objects.filter(
-        title__startswith='Test Title')
+        title__startswith="Test Title")
     test_resources.delete()
 
-    test_users = models.User.objects.filter(username='newUser1')
+    test_users = models.User.objects.filter(username="newUser1")
     test_users.delete()
-    return Response('Test resources and their related comments deleted!',
-                    status=status.HTTP_204_NO_CONTENT)
+    return Response(
+        "Test resources and their related comments deleted!",
+        status=status.HTTP_204_NO_CONTENT,
+    )
