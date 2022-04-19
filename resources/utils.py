5c5afceb5e9dee32dfa04df9ed04aa9e5e19b4e1
Manuel <manuelpinedacabeza@gmail.com>
2022-04-19 17:54:32 +0200

    Reformatted code.

--- resources/utils.py
+++ resources/utils.py
@@ -3,9 +3,10 @@ import json
 
 def read_from_json(file):
     with open(file) as f:
-      data = json.load(f)
+        data = json.load(f)
     return data
 
+
 def get_default_data():
-  data = read_from_json("resources/default-data.json")
-  return data
+    data = read_from_json("resources/default-data.json")
+    return data
