7cc6883787e7e540342ead06f61c5d8ff08e5443
Manuel <manuelpinedacabeza@gmail.com>
2022-04-19 17:52:48 +0200

    Reformatted code.

--- resources/admin.py
+++ resources/admin.py
@@ -6,26 +6,34 @@ from resources import models
 
 # Register your models here.
 
+
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'title', 'author', 'description']
+    list_display = ["pk", "title", "author", "description"]
+
 
 class BookAdmin(ResourceAdmin):
-    list_display = ['pk', 'title', 'author', 'isbn']
+    list_display = ["pk", "title", "author", "isbn"]
+
 
 class PodcastAdmin(ResourceAdmin):
-    list_display = ['pk', 'title', 'author', 'youtube_page_url', 'spotify_page_url']
+    list_display = ["pk", "title", "author", "youtube_page_url", "spotify_page_url"]
+
 
 class PodcastEpisodeAdmin(ResourceAdmin):
-    list_display = ['pk', 'title', 'youtube_episode_url', 'from_podcast']
+    list_display = ["pk", "title", "youtube_episode_url", "from_podcast"]
+
 
 class MotivationalSpeechAdmin(ResourceAdmin):
-    list_display = ['pk', 'title', 'author', 'youtube_url', 'value_one']
+    list_display = ["pk", "title", "author", "youtube_url", "value_one"]
+
 
 class CommentAdmin(ResourceAdmin):
-    list_display = ['pk', 'resource', 'user', 'text', 'date_created']
+    list_display = ["pk", "resource", "user", "text", "date_created"]
+
 
 class RatingAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'resource', 'user', 'stars']
+    list_display = ["pk", "resource", "user", "stars"]
+
 
 admin.site.register(models.Resource, ResourceAdmin)
 admin.site.register(models.Book, BookAdmin)
@@ -34,7 +42,3 @@ admin.site.register(models.PodcastEpisode, PodcastEpisodeAdmin)
 admin.site.register(models.MotivationalSpeech, MotivationalSpeechAdmin)
 admin.site.register(models.Comment, CommentAdmin)
 admin.site.register(models.Rating, RatingAdmin)
-
-
-
-
