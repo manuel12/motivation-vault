03c08e9bb6891aca42d89d68cc51b2d542cb6afd
Manuel <manuelpinedacabeza@gmail.com>
2022-04-19 18:00:25 +0200

    Added use of token.

--- front-end/src/components/Homepage.js
+++ front-end/src/components/Homepage.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import PostList from "./PostList";
 import { API } from "../api-service";
+import useToken from "./useToken";
 
 const HomePage = () => {
+  const { token } = useToken();
   const [resources, setResources] = useState([]);
 
   useEffect(() => {
-    API.fetchAllResources(setResources);
+    API.fetchAllResources(token, setResources);
   }, []);
 
   return (
