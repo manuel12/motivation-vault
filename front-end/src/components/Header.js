21d166a7d5f4ae3d5dac1a4349f3abdd1fa7abbc
Manuel <manuelpinedacabeza@gmail.com>
2022-04-20 23:45:12 +0200

    Updated data atributes.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -3,7 +3,7 @@ import classes from "../css/Header.module.css";
 const Header = (props) => {
   return (
     <div>
-      <header data-test="heading">
+      <header data-test="header">
         <a className={classes["header-link"]} href="/">
           <h1>Motivation Vault</h1>
         </a>
