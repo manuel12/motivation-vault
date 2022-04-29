9267b287f88e5aaf459a06a2473a176ba4799ea1
Manuel <manuelpinedacabeza@gmail.com>
2022-04-29 13:13:25 +0200

    Updated fixture file name. Updated cypress command names. Updated resourceTestData to resourceData.

--- e2e/cypress/integration/api-tests/podcast-api.spec.js
+++ e2e/cypress/integration/api-tests/podcast-api.spec.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-const resourceTestData = require("../../fixtures/resourceAPITestData.json");
+const resourceData = require("../../fixtures/resource-api-data.json");
 const testuserData = require("../../fixtures/testuser.json");
 
 describe("Podacst API 'GET' request", () => {
   before(() => {
     cy.deleteTestData();
-    cy.addResourceWithAPI("podcast", resourceTestData);
+    cy.createResourceWithAPI("podcast", resourceData);
   });
 
   it("should have status code 200", () => {
@@ -49,8 +49,8 @@ describe("Podacst API 'GET' request", () => {
     }).then((response) => {
       const firstPodcast = response.body[0];
       expect(firstPodcast).to.have.property("id");
-      expect(firstPodcast).to.have.property("title", "Test Title");
-      expect(firstPodcast).to.have.property("author", "Test Author");
+      expect(firstPodcast).to.have.property("title", "[Test Title]");
+      expect(firstPodcast).to.have.property("author", "[Test Author]");
       expect(firstPodcast).to.have.property("description");
       expect(firstPodcast).to.have.property("imageURL");
       expect(firstPodcast).to.have.property("website_url");
@@ -71,7 +71,7 @@ describe("Podacst API 'POST' request", () => {
         "Content-Type": "application/json",
         Authorization: `Token  ${testuserData.token}`,
       },
-      body: JSON.stringify(resourceTestData),
+      body: JSON.stringify(resourceData),
     }).then((response) => {
       expect(response.status).to.eq(201);
     });
@@ -85,7 +85,7 @@ describe("Podacst API 'POST' request", () => {
         "Content-Type": "application/json",
         Authorization: `Token  ${testuserData.token}`,
       },
-      body: JSON.stringify(resourceTestData),
+      body: JSON.stringify(resourceData),
     }).then((response) => {
       expect(response.headers).to.have.property(
         "content-type",
@@ -102,22 +102,22 @@ describe("Podacst API 'POST' request", () => {
         "Content-Type": "application/json",
         Authorization: `Token  ${testuserData.token}`,
       },
-      body: JSON.stringify(resourceTestData),
+      body: JSON.stringify(resourceData),
     }).then((response) => {
       const podcast = response.body;
       expect(podcast).to.have.property("id");
-      expect(podcast).to.have.property("title", resourceTestData.title);
-      expect(podcast).to.have.property("author", resourceTestData.author);
+      expect(podcast).to.have.property("title", resourceData.title);
+      expect(podcast).to.have.property("author", resourceData.author);
       expect(podcast).to.have.property("description");
       expect(podcast).to.have.property("imageURL");
-      expect(podcast).to.have.property("website_url", resourceTestData.website_url);
+      expect(podcast).to.have.property("website_url", resourceData.website_url);
       expect(podcast).to.have.property(
         "spotify_page_url",
-        resourceTestData.spotify_page_url
+        resourceData.spotify_page_url
       );
       expect(podcast).to.have.property(
         "youtube_page_url",
-        resourceTestData.youtube_page_url
+        resourceData.youtube_page_url
       );
       expect(podcast).to.have.property("avg_rating", 0);
       expect(podcast).to.have.property("num_ratings", 0);
