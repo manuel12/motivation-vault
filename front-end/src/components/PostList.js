aad41189486481928522f7eaefc67666d8297b8f
Manuel <manuelpinedacabeza@gmail.com>
2022-05-02 15:03:21 +0100

    Added 'No resource data...' fallback.

--- front-end/src/components/PostList.js
+++ front-end/src/components/PostList.js
@@ -1,22 +1,27 @@
 import Post from "./Post";
+import classes from "../css/PostList.module.css";
 
 const PostList = (props) => {
   return (
     <div data-test="post-list-container">
-      {props.resources.map((resource) => (
-        <Post
-          key={resource.id}
-          id={resource.id}
-          title={resource.title}
-          author={resource.author}
-          description={resource.description}
-          imageURL={resource.imageURL}
-          avgRating={resource.avg_rating}
-          numRatings={resource.num_ratings}
-        />
-      ))}
+      {props.resources && props.resources.length > 0 ? (
+        props.resources.map((resource) => (
+          <Post
+            key={resource.id}
+            id={resource.id}
+            title={resource.title}
+            author={resource.author}
+            description={resource.description}
+            imageURL={resource.imageURL}
+            avgRating={resource.avg_rating}
+            numRatings={resource.num_ratings}
+          />
+        ))
+      ) : (
+        <h2>No resoure data...</h2>
+      )}
     </div>
   );
-}
+};
 
 export default PostList;
