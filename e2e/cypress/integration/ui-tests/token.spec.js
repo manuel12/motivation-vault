5a2628b6f6abc9d1b2594ee1c022b1062c0e0fca
Manuel <manuelpinedacabeza@gmail.com>
2022-05-03 17:17:46 +0100

    Added token tests

---
+++ e2e/cypress/integration/ui-tests/token.spec.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress" />
+
+const testuserData = require("../../fixtures/testuser.json");
+
+describe("Token", () => {
+  beforeEach(() => {
+    cy.deleteTestData();
+    cy.visit("/");
+  });
+
+  it("should redirect logged in user to authentication after altering the token", () => {
+    cy.loginWithUI();
+    cy.get("[data-test=homepage]").should(() => {
+      cy.log("Altering the token...")
+
+      localStorage.setItem("token", 12345)
+      let token = JSON.parse(localStorage.getItem("token"))
+      expect(token).to.eq(12345);
+    });
+
+    cy.reload()
+    cy.get("[data-test=login-container]").should("be.visible")
+  });
+
+  it("should redirect logged in user to authentication after deleting the token", () => {
+    cy.loginWithUI();
+    cy.get("[data-test=homepage]").should(() => {
+      cy.log("Deleting the token...")
+
+      delete localStorage.token
+      let token = JSON.parse(localStorage.getItem("token"))
+      expect(token).to.eq(null);
+    })
+
+    cy.reload()
+    cy.get("[data-test=login-container]").should("be.visible")
+  });
+
+  it("should redirect user with an invalid token to authentication", () => {
+    cy.get("[data-test=login-container]").then(() => {
+      cy.log("Adding invalid token...")
+
+      localStorage.setItem("token", "invalid-token12345");
+      let token = localStorage.getItem("token")
+      expect(token).to.eq("invalid-token12345");
+    })
+
+    cy.reload()
+    cy.get("[data-test=login-container]").should("be.visible")
+  });
+});
