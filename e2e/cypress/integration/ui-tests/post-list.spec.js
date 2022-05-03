b6f486d785872c0a09c4807f39e595c7c8f1533b
Manuel <manuelpinedacabeza@gmail.com>
2022-05-03 17:17:02 +0100

    Added test should display 'no resource data' heading.

--- e2e/cypress/integration/ui-tests/post-list.spec.js
+++ e2e/cypress/integration/ui-tests/post-list.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Header", () => {
+describe("Post List", () => {
   beforeEach(() => {
     cy.loginAndCleanUp();
   });
@@ -11,8 +11,13 @@ describe("Header", () => {
       .each(($child) => {
         cy.get($child)
           .invoke("attr", "class")
-          .should("contain", "post-container")
-      })
-  })
-  
-})
+          .should("contain", "post-container");
+      });
+  });
+
+  it("should display 'no resource data' heading", () => {
+    cy.intercept("http://127.0.0.1:8000/api/", []);
+    cy.visit("/");
+    cy.get("h2").should("have.text", "No resoure data...");
+  });
+});
