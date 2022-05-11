fa6ccdd26a0ba21aba62de13b7562b3fe60fb8e9
Manuel <manuelpinedacabeza@gmail.com>
2022-05-11 18:25:38 +0100

    Refactored test.

--- e2e/cypress/integration/ui-tests/post-list.spec.js
+++ e2e/cypress/integration/ui-tests/post-list.spec.js
@@ -5,19 +5,25 @@ describe("Post List", () => {
     cy.loginAndCleanUp();
   });
 
-  it("should display posts on post-list", () => {
-    cy.get("[data-test=post-list-container]")
-      .children()
-      .each(($child) => {
-        cy.get($child)
-          .invoke("attr", "class")
-          .should("contain", "post-container");
-      });
+  it("should display posts on post list element", () => {
+    cy.get("[data-test=spinner]")
+      .should("not.exist").then(() => {
+
+      cy.get("[data-test=post-list-container]")
+        .children()
+        .each(($child) => {
+          cy.get($child).within(($elem) => {
+            cy.get("[data-test=post-image]").should("be.visible")
+            cy.get("[data-test=post-title]").should("be.visible")
+            cy.get("[data-test=post-author]").should("be.visible")
+          })
+        });
+    });
   });
 
-  it("should display 'no resource data' heading", () => {
+  it("should display loading spinner when there is no data", () => {
     cy.intercept("http://127.0.0.1:8000/api/", []);
     cy.visit("/");
-    cy.get("h2").should("have.text", "No resoure data...");
+    cy.get("[data-test=spinner]").should("be.visible");
   });
 });
