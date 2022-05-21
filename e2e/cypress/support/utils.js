21129726d72439ee249afa8bc3fa1d67e2ddd404
Manuel <manuelpinedacabeza@gmail.com>
2022-05-21 17:00:33 +0100

    Added getResourceIdFromUrl, getResourceTypePlural  funcs

--- e2e/cypress/support/utils.js
+++ e2e/cypress/support/utils.js
@@ -1,1 +1,19 @@
-export const getResourceIdFromUrl = (url) => url.pathname.replaceAll("/", "");
+export const getResourceIdFromUrl = (url) => {
+  /**
+   * Resource ID number, used for creating
+   * ratings via API.
+   */
+
+  return url.pathname.replaceAll("/", "");
+};
+
+export const getResourceTypePlural = (resourceType) => {
+  /**
+   * Returns the plural form of any resource type.
+   * Used for url creation.
+   */
+
+  return resourceType === "motivational-speech"
+    ? `${resourceType}es`
+    : `${resourceType}s`;
+};
