a24e50aa54b32adc0212fb1002d84e10e5a62ce6
Manuel <manuelpinedacabeza@gmail.com>
2022-08-18 13:45:44 +0200

    Combined all 3 test cases into 1.

--- e2e/cypress/integration/api-tests/create-api.spec.js
+++ e2e/cypress/integration/api-tests/create-api.spec.js
@@ -12,7 +12,15 @@ const resourceTypes = [
 
 for (const resourceType of resourceTypes) {
   describe(`${resourceType} API 'POST' request`, () => {
-    it("should have status code 201", () => {
+    const ctx = {};
+
+    before(() => {
+      cy.request(
+        `http://localhost:8000/api/${getResourceTypePlural(resourceType)}/`
+      ).then((response) => {
+        ctx.bookResourceCount = response.body.length;
+      });
+
       cy.request({
         method: "POST",
         url: `http://localhost:8000/api/${getResourceTypePlural(
@@ -24,121 +32,80 @@ for (const resourceType of resourceTypes) {
         },
         body: JSON.stringify(resourceAPIData),
       }).then((response) => {
-        expect(response.status).to.eq(201);
+        ctx.response = response;
       });
     });
 
-    it("should add 1 to the book resource count", () => {
-      let bookResourceCount;
+    it(`should have status code 201, add 1 to the book resource count, return JSON and have correct ${resourceType} fields`, () => {
+      // Check status code 200
+      expect(ctx.response.status).to.eq(201);
 
+      // Check resource count increased by 1
       cy.request(
         `http://localhost:8000/api/${getResourceTypePlural(resourceType)}/`
       ).then((response) => {
-        bookResourceCount = response.body.length;
+        expect(response.body.length).to.eq(ctx.bookResourceCount + 1);
       });
 
-      cy.request({
-        method: "POST",
-        url: `http://localhost:8000/api/${getResourceTypePlural(
-          resourceType
-        )}/`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${testuserData.token}`,
-        },
-        body: JSON.stringify(resourceAPIData),
-      });
+      // Retun JSON
+      expect(ctx.response.headers).to.have.property(
+        "content-type",
+        "application/json"
+      );
 
-      cy.request(
-        `http://localhost:8000/api/${getResourceTypePlural(resourceType)}/`
-      ).then((response) => {
-        expect(response.body.length).to.eq(bookResourceCount + 1);
-      });
-    });
+      // Check resource count increased by 1
+      const newestResource = ctx.response.body;
+      expect(newestResource).to.have.property("id");
+      expect(newestResource).to.have.property("title", resourceAPIData.title);
+      expect(newestResource).to.have.property("author", resourceAPIData.author);
+      expect(newestResource).to.have.property("description");
+      expect(newestResource).to.have.property("imageURL");
 
-    it("should return JSON", () => {
-      cy.request({
-        method: "POST",
-        url: `http://localhost:8000/api/${getResourceTypePlural(
-          resourceType
-        )}/`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${testuserData.token}`,
-        },
-        body: JSON.stringify(resourceAPIData),
-      }).then((response) => {
-        expect(response.headers).to.have.property(
-          "content-type",
-          "application/json"
+      // Check ResourceType fields are correct
+      if (resourceType === "book") {
+        expect(newestResource).to.have.property(
+          "subtitle",
+          resourceAPIData.subtitle
         );
-      });
-    });
+        expect(newestResource).to.have.property(
+          "isbn",
+          String(resourceAPIData.isbn)
+        );
+      } else if (resourceType === "podcast") {
+        expect(newestResource).to.have.property(
+          "website_url",
+          resourceAPIData.website_url
+        );
+        expect(newestResource).to.have.property(
+          "spotify_page_url",
+          resourceAPIData.spotify_page_url
+        );
+        expect(newestResource).to.have.property(
+          "youtube_page_url",
+          resourceAPIData.youtube_page_url
+        );
+      } else if (resourceType === "podcast-episode") {
+        expect(newestResource).to.have.property(
+          "from_podcast",
+          resourceAPIData.from_podcast
+        );
+        expect(newestResource).to.have.property(
+          "youtube_episode_url",
+          resourceAPIData.youtube_episode_url
+        );
+        expect(newestResource).to.have.property(
+          "spotify_episode_url",
+          resourceAPIData.spotify_episode_url
+        );
+      } else {
+        expect(newestResource).to.have.property(
+          "youtube_url",
+          resourceAPIData.youtube_url
+        );
+      }
 
-    it(`should have ${resourceType} fields`, () => {
-      cy.request({
-        method: "POST",
-        url: `http://localhost:8000/api/${getResourceTypePlural(
-          resourceType
-        )}/`,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Token  ${testuserData.token}`,
-        },
-        body: JSON.stringify(resourceAPIData),
-      }).then((response) => {
-        const newestResource = response.body;
-        expect(newestResource).to.have.property("id");
-        expect(newestResource).to.have.property("title", resourceAPIData.title);
-        expect(newestResource).to.have.property("author", resourceAPIData.author);
-        expect(newestResource).to.have.property("description");
-        expect(newestResource).to.have.property("imageURL");
-
-        if (resourceType === "book") {
-          expect(newestResource).to.have.property(
-            "subtitle",
-            resourceAPIData.subtitle
-          );
-          expect(newestResource).to.have.property(
-            "isbn",
-            String(resourceAPIData.isbn)
-          );
-        } else if (resourceType === "podcast") {
-          expect(newestResource).to.have.property(
-            "website_url",
-            resourceAPIData.website_url
-          );
-          expect(newestResource).to.have.property(
-            "spotify_page_url",
-            resourceAPIData.spotify_page_url
-          );
-          expect(newestResource).to.have.property(
-            "youtube_page_url",
-            resourceAPIData.youtube_page_url
-          );
-        } else if (resourceType === "podcast-episode") {
-          expect(newestResource).to.have.property(
-            "from_podcast",
-            resourceAPIData.from_podcast
-          );
-          expect(newestResource).to.have.property(
-            "youtube_episode_url",
-            resourceAPIData.youtube_episode_url
-          );
-          expect(newestResource).to.have.property(
-            "spotify_episode_url",
-            resourceAPIData.spotify_episode_url
-          );
-        } else {
-          expect(newestResource).to.have.property(
-            "youtube_url",
-            resourceAPIData.youtube_url
-          );
-        }
-
-        expect(newestResource).to.have.property("avg_rating", 0);
-        expect(newestResource).to.have.property("num_ratings", 0);
-      });
+      expect(newestResource).to.have.property("avg_rating", 0);
+      expect(newestResource).to.have.property("num_ratings", 0);
     });
 
     afterEach(() => {
