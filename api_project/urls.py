ab83b7da83b2a1c2c953dd2db7763de78ada5a1e
Manuel <manuelpinedacabeza@gmail.com>
2022-09-02 19:21:44 +0200

    Imported re_path. Added path to catch all resource urls.

--- api_project/urls.py
+++ api_project/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from api.views import obtain_auth_token
 
@@ -7,4 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("auth/", obtain_auth_token),
+    path("", include("resources.urls")),
+    re_path(r'^(?:.*)/?$', include('resources.urls')),
+
 ]
