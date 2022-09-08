9f3df394a1ab9182b487503217f9af6229d2cc83
Manuel <manuelpinedacabeza@gmail.com>
2022-09-08 21:43:56 +0200

    Removed unnecesary logs. Removed commented out code. Set USE_TZ to False.

--- api_project/settings.py
+++ api_project/settings.py
@@ -124,7 +124,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -152,13 +152,6 @@ if os.getcwd() == "/app":
     ALLOWED_HOSTS = ["*"]
 
     # Static asset configuration
-    #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = "staticfiles"
     STATICFILES_DIRS = (os.path.join(
         BASE_DIR, 'front-end', 'build', 'static'),)
-
-print("**********************************************************************************************************************************8")
-print(f"Settings BASE_DIR: {BASE_DIR}")
-print(f"Settings BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))): {os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}")
-print(f"Settings BASE_DIR = os.path.dirname(os.path.abspath(__file__)): {os.path.dirname(os.path.abspath(__file__))}")
-print(f"Settings os.path.join(BASE_DIR, 'front-end', 'build', 'static'): {os.path.join(BASE_DIR, 'front-end', 'build', 'static')}")
