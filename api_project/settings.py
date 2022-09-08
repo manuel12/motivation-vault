5d715969cf1d0a43afcc76f7985c7700a0511d39
Manuel <manuelpinedacabeza@gmail.com>
2022-09-08 14:52:17 +0200

    Added logs to settings

--- api_project/settings.py
+++ api_project/settings.py
@@ -154,4 +154,7 @@ if os.getcwd() == "/app":
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = "staticfiles"
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+    STATICFILES_DIRS = (os.path.join(
+        BASE_DIR, 'front-end', 'build', 'static'),)
+
+print(f"Settings BASE_DIR: {BASE_DIR}")
