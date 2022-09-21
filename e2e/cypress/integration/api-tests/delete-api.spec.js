30d87f56813c19cd8d78dbe6f92cca117f808a6f
Manuel <manuelpinedacabeza@gmail.com>
2022-09-21 11:59:14 +0200

    Added createPodcastForPodcastEpisodeTests before creating test resource.

--- e2e/cypress/integration/api-tests/delete-api.spec.js
+++ e2e/cypress/integration/api-tests/delete-api.spec.js
@@ -1,65 +1,71 @@
 import { getResourceTypePlural } from "../../support/utils";
 const resourceAPIData = require("../../fixtures/resource-api-data.json");
-const adminuserData = require("../../fixtures/adminuser.json");
 
 const resourceTypes = [
   "book",
   "podcast",
-  // "podcast-episode",
+  "podcast-episode",
   "motivational-speech",
 ];
 
 for (const resourceType of resourceTypes) {
   describe(`${resourceType} API 'DELETE' request`, () => {
     const ctx = {};
-  
+
     beforeEach(() => {
       cy.request(
-        `${Cypress.env('baseUrl')}api/${getResourceTypePlural(resourceType)}/`
+        `${Cypress.env("baseUrl")}api/${getResourceTypePlural(resourceType)}/`
       ).then((response) => {
         ctx.originalResourceCount = response.body.length;
-        cy.log(`ctx.originalResourceCount: ${ctx.originalResourceCount}`)
+        cy.log(`ctx.originalResourceCount: ${ctx.originalResourceCount}`);
       });
-  
+
       cy.deleteTestData();
-      cy.createResourceWithAPI(resourceType, resourceAPIData);
-  
+
+      cy.createPodcastForPodcastEpisodeTests(
+        resourceType,
+        resourceAPIData
+      ).then(() => {
+        cy.createResourceWithAPI(resourceType, resourceAPIData);
+      });
+
       cy.request({
         method: "GET",
-        url: `${Cypress.env('baseUrl')}api/${getResourceTypePlural(resourceType)}/`,
+        url: `${Cypress.env("baseUrl")}api/${getResourceTypePlural(
+          resourceType
+        )}/`,
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Token  ${Cypress.env('adminToken')}`,
+          Authorization: `Token  ${Cypress.env("adminToken")}`,
         },
       }).then((response) => {
         const firstResource = response.body[0];
 
         cy.request({
           method: "DELETE",
-          url: `${Cypress.env('baseUrl')}api/${getResourceTypePlural(resourceType)}/${
-            firstResource.id
-          }/`,
+          url: `${Cypress.env("baseUrl")}api/${getResourceTypePlural(
+            resourceType
+          )}/${firstResource.id}/`,
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Token  ${Cypress.env('adminToken')}`,
+            Authorization: `Token  ${Cypress.env("adminToken")}`,
           },
         }).then((response) => {
           ctx.response = response;
         });
       });
     });
-  
+
     it("should have status code 204 and display resource count back to original count after deleting", () => {
-        // Check status code 204
-        expect(ctx.response.status).to.eq(204);
+      // Check status code 204
+      expect(ctx.response.status).to.eq(204);
 
-        /// Check resource count back to original
-        cy.request(
-          `${Cypress.env('baseUrl')}api/${getResourceTypePlural(resourceType)}/`
-        ).then((response) => {
-          expect(response.body.length).to.eq(ctx.originalResourceCount);
-        });
+      /// Check resource count back to original
+      cy.request(
+        `${Cypress.env("baseUrl")}api/${getResourceTypePlural(resourceType)}/`
+      ).then((response) => {
+        expect(response.body.length).to.eq(ctx.originalResourceCount);
+      });
     });
-  });  
+  });
 }
-
