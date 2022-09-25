e6a0e67907ffbbab93836e169e248ba0b16bfa42
Manuel <manuelpinedacabeza@gmail.com>
2022-09-25 17:16:24 +0200

    Added Header on Navbar component

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,15 +1,21 @@
+import Navbar from "./Navbar";
+import useToken from "./useToken";
+
 import classes from "../css/Header.module.css";
 
 const Header = (props) => {
+  const { token, setToken, deleteToken } = useToken();
+
   return (
-    <div>
-      <header className={classes["header"]} data-test="header">
+    <div className={classes["header"]} data-test="header">
+      <div className={classes["header-title"]} data-test="header-title">
         <a className={classes["header-link"]} href="/">
           <h1>Motivation Vault</h1>
         </a>
-      </header> 
+      </div>
+      <Navbar deleteToken={deleteToken} />
     </div>
   );
-}
+};
 
 export default Header;
