b99ff5aed1ef34e95b8dbd676d844de7fa76f3a2
Manuel <manuelpinedacabeza@gmail.com>
2022-09-26 18:32:46 +0200

    Added image snapshot

--- e2e/cypress/integration/ui-tests/components/page-not-found.spec.js
+++ e2e/cypress/integration/ui-tests/components/page-not-found.spec.js
@@ -8,22 +8,22 @@ describe("404 Page not found", () => {
 
   it("should display a 404 page not found on a non-existent url the books, podcasts, podcast episodes and motivational speech sections", () => {
     cy.visit("/non-existant/url/");
-    cy.get("[data-test=not-found]").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible").matchImageSnapshot();
 
     // Book section
     cy.visit("/books/non-existant/url/");
-    cy.get("[data-test=not-found]").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible").matchImageSnapshot();
 
     // Podcast section
     cy.visit("/podcasts/non-existant/url/");
-    cy.get("[data-test=not-found]").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible").matchImageSnapshot();
 
     // Podcast Episode section
     cy.visit("/podcasts-episodes/non-existant/url/");
-    cy.get("[data-test=not-found]").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible").matchImageSnapshot();
 
     // Motivational Speech section
     cy.visit("/motivational-speeches/non-existant/url/");
-    cy.get("[data-test=not-found]").should("be.visible");
+    cy.get("[data-test=not-found]").should("be.visible").matchImageSnapshot();
   });
 });
