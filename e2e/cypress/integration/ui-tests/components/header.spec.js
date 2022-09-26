98e94235b4cb999c17fa872fe011c20e23728c4f
Manuel <manuelpinedacabeza@gmail.com>
2022-09-26 18:31:21 +0200

    Added image snapshots.

--- e2e/cypress/integration/ui-tests/components/header.spec.js
+++ e2e/cypress/integration/ui-tests/components/header.spec.js
@@ -8,11 +8,14 @@ describe("Header", () => {
   it("should display the correct app name on the header", () => {
     cy.get("[data-test=header]")
       .should("have.text", "Motivation Vault")
+      .matchImageSnapshot();
+
   })
   
   it("should display the correct app name on the header after login", () => {
     cy.loginAndCleanUp();
-    cy.get("[data-test=header]")
+    cy.get("[data-test=header-title]")
       .should("have.text", "Motivation Vault")
+      .matchImageSnapshot();
   })
 })
