7d1aea207964c4b07f1cdc6e6ad6f74c5e047ae9
Manuel <manuelpinedacabeza@gmail.com>
2022-10-06 17:59:13 +0200

    Added spinner component

---
+++ front-end/src/components/Spinner.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import NoResourcesText from "./NoResourcesText";
+
+import classes from "../css/Spinner.module.css";
+
+const Spinner = (props) => {
+  const [showNoResourcesText, setShowNoResourcesText] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`props.resources?.length: ${props.resources?.length}`);
+
+      props.resources?.length < 1
+        ? setShowNoResourcesText(true)
+        : setShowNoResourcesText(false);
+    }, 3000);
+  }, [props.resources]);
+
+  return showNoResourcesText ? (
+    <NoResourcesText />
+  ) : (
+    <FontAwesomeIcon
+      icon={faSpinner}
+      className={classes.spinner}
+      data-test="spinner"
+    />
+  );
+};
+
+export default Spinner;
