5390af60a4fca88439afa58de70f1877e6d9aa64
Manuel <manuelpinedacabeza@gmail.com>
2023-02-10 12:40:40 +0100

    Added anchor element around headertittle.

--- front-end/src/components/HeaderTitle.js
+++ front-end/src/components/HeaderTitle.js
@@ -4,7 +4,9 @@ import classes from "../css/HeaderTitle.module.css";
 const HeaderTitle = () => {
   return (
     <div className={classes["header"]} data-test="header">
-      <h1 className={classes["header-title"]}>Motivation Vault</h1>
+      <a href="/">
+        <h1 className={classes["header-title"]}>Motivation Vault</h1>
+      </a>
     </div>
   );
 };
