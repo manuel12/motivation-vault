b1540f39fff94419fb4876e69c1d052cfa835db7
Manuel <manuelpinedacabeza@gmail.com>
2023-02-10 10:08:59 +0100

    Updated html

--- front-end/src/components/NotFound.js
+++ front-end/src/components/NotFound.js
@@ -2,12 +2,12 @@ import classes from "../css/NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className={classes["not-found-container"]}>
       <h1 className={classes["not-found"]} data-test="not-found">
         404 - Not found :(
       </h1>
     </div>
   );
-}
+};
 
 export default NotFound;
