5ac5f802ac147995b92c87647ffe8411a12db962
Manuel <manuelpinedacabeza@gmail.com>
2023-02-10 16:01:15 +0100

    Added useEffect hook to update display props. Added .alert-displayed class.

--- front-end/src/components/AlertMessage.js
+++ front-end/src/components/AlertMessage.js
@@ -1,19 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from "../css/AlertMessage.module.css";
 
 const AlertMessage = (props) => {
-  const [display, setDisplay] = useState(true);
+  const [display, setDisplay] = useState(props.display);
+
+  useEffect(() => {
+    setDisplay(props.display);
+  }, [props.display]);
 
   const closeButtonClickedHandler = () => {
     setDisplay(false);
   };
 
   return (
-    <div className={display ? classes["alert"] : classes["no-display"]}>
+    <div
+      className={
+        display
+          ? `${classes["alert"]} ${classes["alert-displayed"]}`
+          : `${classes["alert"]}`
+      }
+    >
       <span className={classes["closebtn"]} onClick={closeButtonClickedHandler}>
         &times;
       </span>
-      {props.message}
+      Resource updated successfully!
+      {/*props.message*/}
     </div>
   );
 };
