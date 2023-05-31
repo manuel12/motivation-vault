dfd76d476a26819c21715be058b0b9645d365009
Manuel <manuelpinedacabeza@gmail.com>
2023-05-31 10:53:35 +0200

    Added data test prop

--- front-end/src/components/HeaderTitle.js
+++ front-end/src/components/HeaderTitle.js
@@ -3,9 +3,11 @@ import classes from "../css/HeaderTitle.module.css";
 
 const HeaderTitle = () => {
   return (
-    <div className={classes["header"]} data-test="header">
-      <a href="/">
-        <h1 className={classes["header-title"]}>Motivation Vault</h1>
+    <div className={classes["header"]} data-test='header'>
+      <a href='/'>
+        <h1 className={classes["header-title"]} data-test='header-title'>
+          Motivation Vault
+        </h1>
       </a>
     </div>
   );
