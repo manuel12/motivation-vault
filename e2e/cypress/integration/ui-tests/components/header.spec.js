8975d72d9f5c3e8fe112b7ad16b96b8c768d2cb0
Manuel <manuelpinedacabeza@gmail.com>
2023-05-31 10:49:35 +0200

    Removed empty lines

--- e2e/cypress/integration/ui-tests/components/header.spec.js
+++ e2e/cypress/integration/ui-tests/components/header.spec.js
@@ -9,13 +9,12 @@ describe("Header", () => {
     cy.get("[data-test=header]")
       .should("have.text", "Motivation Vault")
       .matchImageSnapshot();
+  });
 
-  })
-  
   it("should display the correct app name on the header after login", () => {
     cy.loginAndCleanUp();
     cy.get("[data-test=header-title]")
       .should("have.text", "Motivation Vault")
       .matchImageSnapshot();
-  })
-})
+  });
+});
